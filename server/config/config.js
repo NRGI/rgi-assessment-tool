b2ab9ac076880e5fe2b6ab88ee3ddd7cdeaf4b76
Chris Perry <byndcivilization@gmail.com>
2015-04-06 11:40:59 -0400

    SAVEPOINT

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,4 @@
+/*jslint nomen: true */
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 
@@ -5,7 +6,8 @@ module.exports = {
     development: {
         // db: 'mongodb://localhost/rgi2015_dev',
         // db: '@c726.candidate.19.mongolayer.com:10726,c582.candidate.32.mongolayer.com:10582/rgi2015_dev',
-        db: '@c582.candidate.32.mongolayer.com:10582,c726.candidate.19.mongolayer.com:10726/rgi2015_dev?replicaSet=set-54c2868c4ae1de388800b2a3',
+        // db: '@c582.candidate.32.mongolayer.com:10582,c726.candidate.19.mongolayer.com:10726/rgi2015_dev?replicaSet=set-54c2868c4ae1de388800b2a3',
+        db: '@c582.candidate.32.mongolayer.com:10582,c726.candidate.19.mongolayer.com:10726/rgi2015_dev',
         rootPath: rootPath,
         port: process.env.PORT || 3030
     },
