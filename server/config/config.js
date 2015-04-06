9061361e7eb726e7903f4b76ca31bc5b262ab23a
Chris Perry <byndcivilization@gmail.com>
2015-04-06 17:59:14 -0400

    SAVEPOINT

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
         // db: '@c582.candidate.32.mongolayer.com:10582,c726.candidate.19.mongolayer.com:10726/rgi2015_dev?replicaSet=set-54c2868c4ae1de388800b2a3',
         db: '@c582.candidate.32.mongolayer.com:10582,c726.candidate.19.mongolayer.com:10726/rgi2015_dev',
         rootPath: rootPath,
-        port: process.env.PORT || 3030
+        port: process.env.PORT || 80
     },
     production: {
         // db: 'mongodb://localhost/rgi2015_production',
