65c2310d636f92890470c88d8016416cac30745d
Chris Perry <byndcivilization@gmail.com>
2015-04-13 11:01:11 -0400

    updated question admin to flesh out new question creation

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,22 +3,25 @@ var mongoose = require('mongoose'),
     userModel = require('../models/User'),
     questionModel = require('../models/Question'),
     answerModel = require('../models/Answers'),
+    documentModel = require('../models/Documents'),
     assessmentModel = require('../models/Assessment');
 
-module.exports = function (config, user, pass) {
-// module.exports = function(config) {
-    // connect to mongodb
-    // mongoose.connect(config.db);
-    mongoose.connect('mongodb://' + user + ':' + pass + config.db);
+module.exports = function (config, user, pass, env) {
+    // connect to mongo
+    if (env === 'local') {
+        mongoose.connect(config.db);
+    } else {
+        mongoose.connect('mongodb://' + user + ':' + pass + config.db);
+    }
+
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error...'));
     db.once('open', function callback() {
         console.log('rgi2015 db opened');
     });
 
+    // import data
     userModel.createDefaultUsers();
-    // answerModel.createDefaultAnswers();
-    // questionModel.createDefaultQuestions();
+    questionModel.createDefaultQuestions();
     assessmentModel.createDefaultAssessments();
-
 };
