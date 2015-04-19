b70c54b0c68956f719aece5f60e52fa5b4a9d317
hansen <hansentaurus@hotmail.com>
2015-04-20 00:16:03 +0200

    Mendeley Authorization

---
+++ server/config/authMendeley.js
@@ -0,0 +1,41 @@
+'use strict';
+var oauth2 = require('simple-oauth2');
+var mendeley = require('../controllers/mendeley');
+var clientId = 1560;
+var clientCredential = 'WHqPESIwLcyTFMtd';
+var endPoint = 'https://api.mendeley.com';
+
+exports.getToken = function (req, res, next) {
+    if(req.mendeleyTokenExist == false || req.validMendeleyToken == false) {
+        // get token from mendely api
+        var credentials = {
+            'clientID': clientId,
+            'clientSecret': clientCredential,
+            'site': endPoint
+        };
+
+        req.token = null;
+        oauth2(credentials).client.getToken({}, function (error, result) {
+            if (error) {
+                console.log('Access Token Error: ', error.message);
+            } else {
+                console.log('received Token: ', result);
+                req.token = result;
+                req.clientId = clientId;
+                req.clientSecret = clientCredential;
+            }
+
+            next();
+        });
+    }
+    else {
+        if(req.mendeleyTokenExist == true) {
+            console.log('Token already exist');
+        }
+        if(req.validMendeleyToken == true) {
+            console.log('Valid token already exist');
+        }
+
+        next();
+    }
+};
