6e3cadfcc9c30ffe75ca346b484353dd00071447
Chris Perry <byndcivilization@gmail.com>
2015-04-21 10:24:35 -0400

    edited mongo db connections urls

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,12 @@ RUN		rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8
 RUN		yum install -y npm
 RUN		yum install -y git
 RUN		npm install -g bower
-RUN     npm build
 
 # Build src
 RUN		git clone https://github.com/NRGI/rgi-assessment-tool /rgi-assessment-tool
 RUN		cd /rgi-assessment-tool && npm install --production
 RUN		cd /rgi-assessment-tool && bower install --allow-root
+RUN     npm build
 
 EXPOSE  80
 
--- server/config/config.js
+++ server/config/config.js
@@ -9,12 +9,12 @@ module.exports = {
         port: process.env.PORT || 3030
     },
     development: {
-        db: '@c582.candidate.32.mongolayer.com:10582,c726.candidate.19.mongolayer.com:10726/rgi2015_dev',
+        db: '@candidate.32.mongolayer.com:10582/rgi2015_dev',
         rootPath: rootPath,
         port: process.env.PORT || 80
     },
     production: {
-        db: '@c726.candidate.19.mongolayer.com:10726,c582.candidate.32.mongolayer.com:10582/rgi2015_production',
+        db: '@candidate.32.mongolayer.com:10582/rgi2015_production',
         rootPath: rootPath,
         port: process.env.PORT || 80
     }
