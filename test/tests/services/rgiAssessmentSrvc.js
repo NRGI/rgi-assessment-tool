c791959f797c7836013562b2207e7e1d80060706
Alex Elgin <sascha.elgin@mail.ru>
2015-06-05 21:55:38 +0600

    add a test suite on assessment service

---
+++ test/tests/services/rgiAssessmentSrvc.js
@@ -0,0 +1,24 @@
+'use strict';
+/*jslint nomen: true newcap: true */
+var describe, beforeEach, it, inject, expect;
+
+describe('rgiAssessmentSrvc', function () {
+    var assessmentServiceInstance, $httpBackend;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function(rgiAssessmentSrvc, _$httpBackend_) {
+        $httpBackend = _$httpBackend_;
+        assessmentServiceInstance = new rgiAssessmentSrvc();
+    }));
+
+    it('requests assessments data for a given `assessmentId`', function () {
+        var assessmentId = 1;
+        $httpBackend.expectGET('/api/assessments/' + assessmentId).respond([]);
+        assessmentServiceInstance.$query({assessment_ID: assessmentId});
+    });
+
+    afterEach(function() {
+        $httpBackend.flush();
+    });
+});
