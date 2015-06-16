118bdaa65d25160a16c3f0fb050b0e038a9dfd94
Alex Elgin <sascha.elgin@mail.ru>
2015-06-16 21:54:08 +0600

    add a test suite on user method service

---
+++ test/tests/services/rgiUserMethodSrvc.js
@@ -0,0 +1,138 @@
+'use strict';
+/*jslint nomen: true newcap: true */
+var describe, beforeEach, it, inject, expect;
+
+describe('rgiUserMethodSrvc', function () {
+    beforeEach(module('app'));
+
+    var rgiUserMethodSrvc;
+    var $q, rgiUserSrvc;
+    var $qDeferStub, $qDeferSpy, expectedPromise;
+
+    beforeEach(inject(function (_rgiUserMethodSrvc_, _$q_, _rgiUserSrvc_) {
+        $q = _$q_;
+        rgiUserSrvc = _rgiUserSrvc_;
+        rgiUserMethodSrvc = _rgiUserMethodSrvc_;
+    }));
+
+    describe('#createUser', function () {
+        var rgiUserSrvcStub;
+
+        it('resolves the deferred in positive case', function () {
+            expectedPromise = 'POSITIVE';
+
+            rgiUserSrvcStub = sinon.stub(rgiUserSrvc.prototype, '$save', function() {
+                return {
+                    then: function(callback) {
+                        callback();
+                    }
+                };
+            });
+
+            $qDeferSpy = sinon.spy();
+            $qDeferStub = sinon.stub($q, 'defer', function() {
+                return {
+                    resolve: $qDeferSpy,
+                    promise: expectedPromise
+                };
+            });
+
+            rgiUserMethodSrvc.createUser([expectedPromise]).should.be.equal(expectedPromise);
+            $qDeferSpy.called.should.be.equal(true);
+        });
+
+        it('resolves the deferred in positive case', function () {
+            expectedPromise = 'NEGATIVE';
+            var REJECT_RESPONSE = {
+                data: {reason: 'REJECT'}
+            };
+
+            rgiUserSrvcStub = sinon.stub(rgiUserSrvc.prototype, '$save', function() {
+                return {
+                    then: function(callbackPositive, callbackNegative) {
+                        callbackNegative(REJECT_RESPONSE);
+                    }
+                };
+            });
+
+            $qDeferSpy = sinon.spy();
+            $qDeferStub = sinon.stub($q, 'defer', function() {
+                return {
+                    reject: $qDeferSpy,
+                    promise: expectedPromise
+                };
+            });
+
+            rgiUserMethodSrvc.createUser([expectedPromise]).should.be.equal(expectedPromise);
+            $qDeferSpy.should.have.been.calledWith(REJECT_RESPONSE.data.reason);
+        });
+
+        afterEach(function () {
+            $qDeferStub.restore();
+            rgiUserSrvcStub.restore();
+        });
+    });
+
+    describe('#deleteUser', function () {
+        var rgiUserSrvcStub;
+
+        it('resolves the deferred in positive case', function () {
+            expectedPromise = 'POSITIVE';
+
+            rgiUserSrvcStub = sinon.stub(rgiUserSrvc.prototype, '$delete', function() {
+                return {
+                    then: function(callback) {
+                        callback();
+                    }
+                };
+            });
+
+            $qDeferSpy = sinon.spy();
+            $qDeferStub = sinon.stub($q, 'defer', function() {
+                return {
+                    resolve: $qDeferSpy,
+                    promise: expectedPromise
+                };
+            });
+
+            rgiUserMethodSrvc.deleteUser([expectedPromise]).should.be.equal(expectedPromise);
+            $qDeferSpy.called.should.be.equal(true);
+        });
+
+        afterEach(function () {
+            $qDeferStub.restore();
+            rgiUserSrvcStub.restore();
+        });
+    });
+
+    describe('#updateUser', function () {
+        it('resolves the deferred in positive case', function () {
+            expectedPromise = 'POSITIVE';
+
+            $qDeferSpy = sinon.spy();
+            $qDeferStub = sinon.stub($q, 'defer', function() {
+                return {
+                    resolve: $qDeferSpy,
+                    promise: expectedPromise
+                };
+            });
+
+            rgiUserMethodSrvc.updateUser({
+                $update: function() {
+                    return {
+                        then: function(callbackPositive) {
+                            callbackPositive();
+                        }
+                    };
+                }
+            }).should.be.equal(expectedPromise);
+
+            $qDeferSpy.called.should.be.equal(true);
+        });
+
+        afterEach(function () {
+            $qDeferStub.restore();
+        });
+    });
+
+});
