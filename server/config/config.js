723fc7f60d0c53e06b55460e7f78abb6a5dea586
Chris Perry <byndcivilization@gmail.com>
2015-07-08 23:05:58 -0400

    SAVEPOINT

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
         port: process.env.PORT || 3030
     },
     development: {
-        db: '@candidate.32.mongolayer.com:10582/rgi_dev',
+        db: '@c726.candidate.19.mongolayer.com:10726/rgi_dev',
         rootPath: rootPath,
         port: process.env.PORT || 80
     },
