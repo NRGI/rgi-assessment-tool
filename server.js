b487c77f45acf682de506cd7759287b1700d2174
Chris Perry <byndcivilization@gmail.com>
2015-08-20 11:06:55 -0400

    SAVEPOINT

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ if (env === 'production' || env === 'pilot') {
 var express         = require('express');
 
 var env             = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-// var env             = 'development';
 
 var app             = express();
 
@@ -16,14 +15,6 @@ var config          = require('./server/config/config')[env];
 
 var user = process.env.DB_ID;
 var pass = process.env.DB_KEY;
-
-// if (env === 'development' || env === 'local') {
-//     var user = process.env.USER_DEV_ID;
-//     var pass = process.env.USER_DEV_KEY;
-// } else if (env === 'production' || env === 'pilot') {
-//     var user = process.env.USER_PROD_ID;
-//     var pass = process.env.USER_PROD_KEY;
-// }
 
 require('./server/config/express')(app, config);
 
@@ -34,5 +25,5 @@ require('./server/config/passport')();
 require('./server/config/routes')(app);
 
 app.listen(config.port);
-// app.listen(3030);
+
 console.log('Listening on port ' + config.port + '...');
