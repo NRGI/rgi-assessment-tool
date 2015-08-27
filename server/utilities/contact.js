9deaa987a40db12b716ee71a5f2ba66b46978dcd
Chris Perry <byndcivilization@gmail.com>
2015-08-27 15:51:36 -0400

    fixed edit button not rendering correctly on user admin page

--- server/utilities/contact.js
+++ server/utilities/contact.js
@@ -35,7 +35,7 @@ exports.techSend = function (req, res) {
     mandrill('/messages/send', {
         message: {
             to: [{email: 'tech-support@resourcegovernance.org', name: 'Assessment tool tech support'}],
-            from_email: message_content.email,
+            from_email: [{email: message_content.email, name: issue_full_name}],
             subject: issue_tool + ' Issue: ' + message_content.issue.value,
             html: "Hi,<p>"
             + issue_full_name + " is having an issue with the " + issue_tool + " tool. Please find more info below.<p>"
@@ -49,11 +49,32 @@ exports.techSend = function (req, res) {
         //everything's good, lets see what mandrill said
         else console.log(response);
     });
+
+    //send a confirmation e-mail to user
+    mandrill('/messages/send', {
+        message: {
+            to: [{email: message_content.email, name: issue_full_name}],
+            from_email: [{email: 'tech-support@resourcegovernance.org', name: 'Assessment tool tech support'}],
+            subject: 'Confirmation of ' + message_content.issue.value + ' support ticket'
+            html: "Hi " + issue_full_name + ",<p>"
+            + "Thanks for contacting us about an issue with the " + issue_tool + "tool. I'm sorry for the inconvenience, we will contact you shortly. "
+            + "In the meantime please look at the info you supplied below and let us know if any of it is incorrect.<p>"
+            + "<ul><li><b>Issue</b>: " + message_content.issue.name + "</li><<li><b>Issue description</b>: " + message_content.issue_description + "</li>"
+            + "<li><b>OS</b>: " + issue_os + "</li><li><b>Browser</b>: " + issue_browser + "</li><li><b>Browser version</b>: " + issue_browser_ver + "</li></ul>"
+        }
+    }, function (error, response) {
+        //uh oh, there was an error
+        if (error) console.log( JSON.stringify(error) );
+
+        //everything's good, lets see what mandrill said
+        else console.log(response);
+    });
     res.send();
 };
 
-exports.contactSend = function (req, res, next) {
-    console.log(req);
+exports.new_user_confirmation = function (contact_packet) {
+    console.log(contact_packet);
+
 //
 //    // send email to new user
 //    mandrill('/messages/send', {
