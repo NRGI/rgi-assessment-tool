98d5f5ed03e3c8081a670cd103630db7228ce029
Chris Perry <byndcivilization@gmail.com>
2015-09-01 11:04:11 -0400

    added flag editing

--- public/app/researchers/answers/rgiAnswerCtrl.js
+++ public/app/researchers/answers/rgiAnswerCtrl.js
@@ -189,6 +189,19 @@ angular.module('app').controller('rgiAnswerCtrl', function ($scope, $routeParams
         });
     };
 
+    $scope.flagEdit = function (flag, index) {
+        $scope.value = true;
+        var scope = $scope;
+        scope.index = index;
+        scope.flag = flag;
+        ngDialog.open({
+            template: 'partials/dialogs/flag-answer-dialog',
+            controller: 'rgiFlagEditDialogCtrl',
+            className: 'ngdialog-theme-default',
+            scope: scope
+        });
+    };
+
     $scope.answerResubmit = function () {
         var new_answer_data, new_assessment_data;
 
