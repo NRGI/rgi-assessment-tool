53160c76bd35015b55e3ccf69c223434de21ba9f
Alex Elgin <sascha.elgin@gmail.com>
2015-09-08 22:39:50 +0600

    compile Jade files using `grunt jade`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,25 @@
+/*jslint node: true */
 'use strict';
 module.exports = function (grunt) {
-
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jade: {
+            compile: {
+                options: {
+                    client: false
+                },
+                files: [
+                    {
+                        cwd: "public/app",
+                        src: "**/*.jade",
+                        dest: "public/app",
+                        expand: true,
+                        ext: ".html"
+                    }
+                ]
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -15,10 +31,9 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jade');
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
-
 };
