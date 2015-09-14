f54a011a880fff65899d8a8deedf70ff28dd6926
Chris Perry <byndcivilization@gmail.com>
2015-09-14 16:28:14 -0400

    SAVEPOINT

--- server/models/Answers.js
+++ server/models/Answers.js
@@ -56,6 +56,23 @@ var humanSchema = new mongoose.Schema({
     }
 });
 
+//var interviewSchema = new mongoose.Schema({
+//    interviewee_ID: ObjectId,
+//    contact_date: {
+//        type: Date,
+//        default: Date.now},
+//    comment: {
+//        date: {
+//            type: Date,
+//            default: Date.now},
+//        content: String,
+//        author: ObjectId, // Pull from curretn user _id value
+//        author_name: String,
+//        role: String,
+//        addressed: Boolean
+//    }
+//});
+
 var interviewSchema = new mongoose.Schema({
     interviewee_ID: ObjectId,
     contact_date: {
