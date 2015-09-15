e56435e30b4c9b0c1fefdf2cba12aa0bbc2898fa
Alex Elgin <sascha.elgin@gmail.com>
2015-09-15 20:16:40 +0600

    remove token on password reset

--- server/controllers/reset-password-tokens.js
+++ server/controllers/reset-password-tokens.js
@@ -5,36 +5,32 @@ var
     ResetPasswordToken = require('mongoose').model('ResetPasswordToken'),
     User = require('mongoose').model('User');
 
-exports.create = function (req, res, next) {
+exports.create = function (req, res) {
     User.findOne({email: req.body.email}, function(findUserError, user) {
         if (findUserError || !user) {
-            next(findUserError);
             res.send({error: 'USER_NOT_FOUND'});
         } else {
             ResetPasswordToken.createByUser(user._id, function(error, token) {
-                if (error) next(error);
                 res.send({error: error, token: token});
             });
         }
     });
 };
 
-exports.reset = function (req, res, next) {
+exports.reset = function (req, res) {
     ResetPasswordToken.findOne({_id: req.body.token}, function (findTokenError, token) {
         if (findTokenError || !token) {
-            next(findTokenError);
             res.send({error: 'TOKEN_NOT_FOUND'});
         } else {
             User.findOne({_id: token.user}, function(findUserError, user) {
                 if (findUserError || !user) {
-                    next(findUserError);
                     res.send({error: 'USER_NOT_FOUND'});
                 } else {
                     user.setPassword(req.body.password, function(setPasswordError) {
                         if (setPasswordError) {
-                            next(setPasswordError);
                             res.send({error: 'SET_PASSWORD_ERROR'});
                         } else {
+                            token.remove(function() {});
                             res.send({});
                         }
                     });
