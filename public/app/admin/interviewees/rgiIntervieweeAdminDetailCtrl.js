ceb6377843c0aa3494fb1f7615c54cf71ab9158e
Chris Perry <byndcivilization@gmail.com>
2015-09-15 18:06:31 -0400

    brought over interviewee management

---
+++ public/app/admin/interviewees/rgiIntervieweeAdminDetailCtrl.js
@@ -0,0 +1,49 @@
+'use strict';
+/*jslint nomen: true unparam: true regexp: true*/
+//var angular;
+
+angular.module('app').controller('rgiIntervieweeAdminDetailCtrl', function ($scope, $route, $routeParams, ngDialog, rgiNotifier, rgiIntervieweeSrvc, rgiIntervieweeMethodSrvc, rgiAssessmentSrvc) {
+    rgiAssessmentSrvc.query({}, function (assessments) {
+        rgiIntervieweeSrvc.get({_id: $routeParams.interviewee_ID}, function (interviewee) {
+            $scope.interviewee = interviewee;
+            $scope.assessments = [];
+            assessments.forEach(function (el) {
+                if (interviewee.assessments.indexOf(el.assessment_ID) < 0) {
+                    $scope.assessments.push({
+                        assessment_ID: el.assessment_ID,
+                        text: el.country + ' - ' + el.year
+                    });
+                }
+            });
+
+        });
+
+    });
+
+    $scope.editIntervieweeDialog = function () {
+        $scope.value = true;
+        ngDialog.open({
+            template: 'partials/dialogs/edit-interviewee-dialog',
+            controller: 'rgiEditIntervieweeDialogCtrl',
+            className: 'ngdialog-theme-default',
+            scope: $scope
+        });
+    };
+
+    $scope.addAssessment = function () {
+        if ($scope.add_assessment === undefined) {
+            rgiNotifier.error('You must select an assessment from the dropdown!');
+        } else {
+            var new_interviewee_data = $scope.interviewee;
+            if (new_interviewee_data.assessments.indexOf($scope.add_assessment) < 0) {
+                new_interviewee_data.assessments.push($scope.add_assessment);
+                rgiIntervieweeMethodSrvc.updateInterviewee(new_interviewee_data).then(function () {
+                    rgiNotifier.notify('Interviewee updated');
+                    $route.reload();
+                }, function (reason) {
+                    rgiNotifier.notify(reason);
+                });
+            }
+        }
+    };
+});
