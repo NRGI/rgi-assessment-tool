51156dd4f91566606ca0dcb7fbdfb89d617276f0
Alex Elgin <sascha.elgin@gmail.com>
2015-09-16 13:06:07 +0600

    add 'Reset Password' page

--- public/app/app.js
+++ public/app/app.js
@@ -38,6 +38,10 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             controller:  'rgiProfileCtrl',
             resolve: routeRoleChecks.user
         })
+        .when('/reset-password/:token', {
+            templateUrl: '/partials/account/reset-password',
+            controller:  'rgiResetPasswordCtrl'
+        })
 
         ///// Admin Routes
         // USERS
