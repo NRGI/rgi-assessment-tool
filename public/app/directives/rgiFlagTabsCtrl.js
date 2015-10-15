acb021523a8719334f2b43366506bea733ca1e04
Chris Perry <byndcivilization@gmail.com>
2015-10-14 23:27:11 -0400

    flag edit working

--- public/app/directives/rgiFlagTabsCtrl.js
+++ public/app/directives/rgiFlagTabsCtrl.js
@@ -1,17 +1,22 @@
 'use strict';
 
-angular.module('app').controller('rgiFlagTabsCtrl', function ($scope) {
-    $scope.flagEdit = function (flag, index) {
-        //$scope.value = true;
-        //var scope = $scope;
-        //scope.index = index;
-        //scope.flag = flag;
-        //ngDialog.open({
-        //    template: 'partials/dialogs/flag-answer-dialog',
-        //    controller: 'rgiFlagEditDialogCtrl',
-        //    className: 'ngdialog-theme-default dialogwidth800',
-        //    scope: scope
-        //});
-    };
+angular
+    .module('app')
+    .controller('rgiFlagTabsCtrl', function (
+        $scope,
+        ngDialog
+    ) {
+        $scope.flagEdit = function (flag, index) {
+            $scope.value = true;
+            var scope = $scope;
+            scope.index = index;
+            scope.flag = flag;
+            ngDialog.open({
+                template: 'partials/dialogs/flag-answer-dialog',
+                controller: 'rgiFlagEditDialogCtrl',
+                className: 'ngdialog-theme-default dialogwidth800',
+                scope: scope
+            });
+        };
 
-});
+    });
