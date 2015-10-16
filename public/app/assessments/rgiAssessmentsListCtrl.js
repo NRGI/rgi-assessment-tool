e7366470f8f4e16a178e8c31597ab108ae82a4f8
Chris Perry <byndcivilization@gmail.com>
2015-10-16 17:16:39 -0400

    added button functinality to user assessment list table buttons

--- public/app/assessments/rgiAssessmentsListCtrl.js
+++ public/app/assessments/rgiAssessmentsListCtrl.js
@@ -54,31 +54,31 @@ angular
             });
         }
 
-        $scope.assessmentStart = function (assessment) {
-            var new_assessment_data = assessment;
-
-            new_assessment_data.status = 'started';
-            new_assessment_data.start_date = {started_by: rgiIdentitySrvc.currentUser._id};
-
-            rgiAssessmentMethodSrvc.updateAssessment(new_assessment_data).then(function () {
-                $location.path('/assessments/answer/' + new_assessment_data.assessment_ID + '-001');
-                rgiNotifier.notify('Assessment started!');
-            }, function (reason) {
-                rgiNotifier.error(reason);
-            });
-        };
-
-        $scope.assessmentResubmit = function () {
-            var new_assessment_data = new rgiAssessmentSrvc($scope.assessment);
-
-            new_assessment_data.status = 'resubmitted';
-
-            rgiAssessmentMethodSrvc.updateAssessment(new_assessment_data)
-                .then(function () {
-                    $location.path('/assessments');
-                    rgiNotifier.notify('Assessment submitted!');
-                }, function (reason) {
-                    rgiNotifier.error(reason);
-                });
-        };
+        //$scope.assessmentStart = function (assessment) {
+        //    var new_assessment_data = assessment;
+        //
+        //    new_assessment_data.status = 'started';
+        //    new_assessment_data.start_date = {started_by: rgiIdentitySrvc.currentUser._id};
+        //
+        //    rgiAssessmentMethodSrvc.updateAssessment(new_assessment_data).then(function () {
+        //        $location.path('/assessments/answer/' + new_assessment_data.assessment_ID + '-001');
+        //        rgiNotifier.notify('Assessment started!');
+        //    }, function (reason) {
+        //        rgiNotifier.error(reason);
+        //    });
+        //};
+        //
+        //$scope.assessmentResubmit = function () {
+        //    var new_assessment_data = new rgiAssessmentSrvc($scope.assessment);
+        //
+        //    new_assessment_data.status = 'resubmitted';
+        //
+        //    rgiAssessmentMethodSrvc.updateAssessment(new_assessment_data)
+        //        .then(function () {
+        //            $location.path('/assessments');
+        //            rgiNotifier.notify('Assessment submitted!');
+        //        }, function (reason) {
+        //            rgiNotifier.error(reason);
+        //        });
+        //};
     });
