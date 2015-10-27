c4a0d4cc360a7fd42a2d62a21207daf1fd163c25
Chris Perry <byndcivilization@gmail.com>
2015-10-27 17:12:59 -0600

    added error checks for profile change

--- public/app/account/rgiProfileCtrl.js
+++ public/app/account/rgiProfileCtrl.js
@@ -11,31 +11,30 @@ angular
         // set page resources to be those of the current identity
         $scope.fullName = rgiIdentitySrvc.currentUser.firstName + " " + rgiIdentitySrvc.currentUser.lastName;
         $scope.current_user = rgiIdentitySrvc.currentUser;
-        //$scope.first_name = rgiIdentitySrvc.currentUser.firstName;
-        //$scope.last_name = rgiIdentitySrvc.currentUser.lastName;
-        //$scope.email = rgiIdentitySrvc.currentUser.email;
-        //$scope.username = rgiIdentitySrvc.currentUser.username;
-        //$scope.role = rgiIdentitySrvc.currentUser.role;
-        //$scope.address = rgiIdentitySrvc.currentUser.address;
 
         // update functinonality for update button
         $scope.update = function () {
-            // pass in update data
             var new_user_data = $scope.current_user;
-            // check if password update exists and pass it in
-            if ($scope.password && $scope.password.length > 0) {
-                if ($scope.password !== $scope.password_rep) {
-                    rgiNotifier.error('Passwords must match!');
-                } else {
-                    new_user_data.password = $scope.password;
+            if (!new_user_data.firstName || !new_user_data.lastName) {
+                rgiNotifier.error('You must supply a first and last name!');
+            } else if (!new_user_data.email) {
+                rgiNotifier.error('You must supply an email!');
+            } else {
+                // check if password update exists and pass it in
+                if ($scope.password && $scope.password.length > 0) {
+                    if ($scope.password !== $scope.password_rep) {
+                        rgiNotifier.error('Passwords must match!');
+                    } else {
+                        new_user_data.password = $scope.password;
+                    }
                 }
+                // use authorization service to update user data
+                rgiUserMethodSrvc.updateUser(new_user_data).then(function () {
+                    rgiNotifier.notify('Your user account has been updated');
+                    $route.reload();
+                }, function (reason) {
+                    rgiNotifier.error(reason);
+                });
             }
-            // use authorization service to update user data
-            rgiUserMethodSrvc.updateUser(new_user_data).then(function () {
-                rgiNotifier.notify('Your user account has been updated');
-                $route.reload();
-            }, function (reason) {
-                rgiNotifier.error(reason);
-            });
         };
     });
