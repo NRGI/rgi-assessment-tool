b3f798de8286febaae7296b210f20438a16f0957
Alex Elgin <sascha.elgin@gmail.com>
2015-12-14 16:40:16 +0600

    introduce a service to define next question

---
+++ public/app/services/rgiQuestionSetSrvc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('app').factory('rgiQuestionSetSrvc', function (rgiUtilsSrvc) {
+    var answers = [];
+
+    return {
+        areQuestionsRemaining: function(answer) {
+            return answer.question_order !== answers.length;
+        },
+        getNextQuestionId: function(answer) {
+            return String(rgiUtilsSrvc.zeroFill((answer.question_order + 1), 3));
+        },
+        setAnswers: function(answersData) {
+            answers = answersData;
+        }
+    };
+});
