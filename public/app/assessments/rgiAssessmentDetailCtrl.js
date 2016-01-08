cf9a5f2282c2fd1578482e1f04215647d2d9301a
Chris Perry <byndcivilization@gmail.com>
2016-01-08 17:41:25 -0500

    added accordion for precepts: addresses #111

--- public/app/assessments/rgiAssessmentDetailCtrl.js
+++ public/app/assessments/rgiAssessmentDetailCtrl.js
@@ -47,7 +47,104 @@ angular
                     finalized: 0
                 };
 
-                $scope.answers = [];
+                $scope.answers = {
+                    precept_1: {
+                        label: "Precept 1: Strategy, consultation and institutions",
+                        id: "precept_1",
+                        order: 1,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_2: {
+                        label: "Precept 2: Accountability and transparency",
+                        id: "precept_2",
+                        order: 2,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_3: {
+                        label: "Precept 3: Exploration and license allocation",
+                        id: "precept_3",
+                        order: 3,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_4: {
+                        label: "Precept 4: Taxation",
+                        id: "precept_4",
+                        order: 4,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_5: {
+                        label: "Precept 5: Local effects",
+                        id: "precept_5",
+                        order: 5,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_6: {
+                        label: "Precept 6: State-owned enterprise",
+                        id: "precept_6",
+                        order: 6,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_7: {
+                        label: "Precept 7: Revenue distribution",
+                        id: "precept_7",
+                        order: 7,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_8: {
+                        label: "Precept 8: Revenue volatility",
+                        id: "precept_8",
+                        order: 8,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_9: {
+                        label: "Precept 9: Government spending",
+                        id: "precept_9",
+                        order: 9,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_10: {
+                        label: "Precept 10: Private sector development",
+                        id: "precept_10",
+                        order: 10,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_11: {
+                        label: "Precept 11: Roles of international companies",
+                        id: "precept_11",
+                        order: 11,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    },
+                    precept_12: {
+                        label: "Precept 12: Roles of international actors",
+                        id: "precept_12",
+                        order: 12,
+                        section_len: 0,
+                        complete: 0,
+                        data: []
+                    }
+                };
 
                 answers.forEach(function (answer) {
                     switch (answer.status) {
@@ -88,9 +185,16 @@ angular
                         answer.question_order = question.question_order;
                         answer.question_label = question.question_label;
                         answer.component_text = question.component_text;
+                        answer.precept = question.precept;
                         answer.component_id = question.component_id;
+                        //answer.status
+                        $scope.answers["precept_" + String(answer.precept)].section_len += 1;
+                        if (answer.status === 'submitted') {
+                            $scope.answers["precept_" + String(answer.precept)].complete += 1;
+                        }
+                        $scope.answers["precept_" + String(answer.precept)].data.push(answer);
+
                     });
-                    $scope.answers.push(answer);
                 });
             });
         });
