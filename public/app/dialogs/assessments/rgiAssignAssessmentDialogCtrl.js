b28aba2cb364c990af9002f00f79e7da5d393735
Chris Perry <byndcivilization@gmail.com>
2016-01-14 17:03:29 -0500

    worked on reasign functino

--- public/app/dialogs/assessments/rgiAssignAssessmentDialogCtrl.js
+++ public/app/dialogs/assessments/rgiAssignAssessmentDialogCtrl.js
@@ -23,14 +23,19 @@ angular
         $scope.reviewers = rgiUserSrvc.query({role: 'reviewer'});
 
         // get assessment that needs to be updated
-        $scope.assessment = rgiAssessmentSrvc.get({assessment_ID: $scope.$parent.assessment_update_ID});
+        rgiAssessmentSrvc.get({assessment_ID: $scope.$parent.assessment_update_ID}, function(assessment) {
+            $scope.assessment = assessment;
+            $scope.researcher_select = rgiUserSrvc.get({_id: assessment.researcher_ID});
+            $scope.reviewer_select = rgiUserSrvc.get({_id: assessment.reviewer_ID});
+        });
+
 
         //// get questions for insertion into answers collection
         //$scope.questions = rgiQuestionSrvc.query({assessments: $scope.$parent.assessment_update_ID.substr(3)});
 
-        $scope.assessmentAssign = function (researcherSelect, reviewerSelect) {
+        $scope.assessmentAssign = function (researcher_select, reviewer_select) {
             // update users
-            var new_researcher_data = new rgiUserSrvc(JSON.parse(researcherSelect)),
+            var new_researcher_data = new rgiUserSrvc(JSON.parse(researcher_select)),
                 new_assessment_data = $scope.assessment;
 
             //MAIL NOTIFICATION
@@ -43,15 +48,14 @@ angular
             new_researcher_data.assessments.push({assessment_ID: $scope.$parent.assessment_update_ID, country_name: $scope.assessment.country, year: $scope.assessment.year, version: $scope.assessment.version});
 
             //IF REVIEWER SELECTED
-            if (reviewerSelect) {
-                var new_reviewer_data = new rgiUserSrvc(JSON.parse(reviewerSelect));
+            if (reviewer_select) {
+                var new_reviewer_data = new rgiUserSrvc(JSON.parse(reviewer_select));
 
                 new_assessment_data.reviewer_ID = new_reviewer_data._id;
                 new_reviewer_data.assessments.push({assessment_ID: $scope.$parent.assessment_update_ID, country_name: $scope.assessment.country, year: $scope.assessment.year, version: $scope.assessment.version});
                 rgiUserMethodSrvc.updateUser(new_researcher_data)
                     .then(rgiUserMethodSrvc.updateUser(new_reviewer_data))
                     .then(rgiAssessmentMethodSrvc.updateAssessment(new_assessment_data))
-                    //.then(rgiAnswerMethodSrvc.insertAnswerSet(new_answer_set))
                     .then(function () {
                         rgiNotifier.notify('Assessment assigned!');
                         $location.path('/');
@@ -60,10 +64,9 @@ angular
                     }, function (reason) {
                         rgiNotifier.error(reason);
                     });
-            } else if (!reviewerSelect) {
+            } else if (!reviewer_select) {
                 rgiUserMethodSrvc.updateUser(new_researcher_data)
                     .then(rgiAssessmentMethodSrvc.updateAssessment(new_assessment_data))
-                    //.then(rgiAnswerMethodSrvc.insertAnswerSet(new_answer_set))
                     .then(function () {
                         rgiNotifier.notify('Assessment assigned!');
                         $location.path('/');
@@ -84,7 +87,19 @@ angular
             //}
         };
 
-        $scope.assessmentReassign = function () {
+        $scope.assessmentReassign = function (researcher_select, reviewer_select) {
+            console.log(researcher_select);
+            //if (!researcherSelect) {
+            //    rgiNotifier.error('No researcher data!');
+            //} else {
+            //    var new_researcher_data = new rgiUserSrvc(JSON.parse(researcherSelect)),
+            //        new_assessment_data = $scope.assessment;;
+            //
+            //    if (new_researcher_data._id===new_assessment_data.researcher_ID) {
+            //        $scope.closeThisDialog();
+            //    }
+            //
+            //}
 
         };
 
