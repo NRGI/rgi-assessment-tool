da6c3f4c0dc726072873c1d49cf67c07d44a59cb
Alex Elgin <sascha.elgin@gmail.com>
2016-02-13 20:42:41 +0600

    refactor assessment list controller

--- public/app/assessments/rgiAssessmentsListCtrl.js
+++ public/app/assessments/rgiAssessmentsListCtrl.js
@@ -4,15 +4,11 @@ angular
     .module('app')
     .controller('rgiAssessmentsListCtrl', function (
         $scope,
-        $location,
         $routeParams,
         rgiDialogFactory,
-        ngDialog,
-        rgiNotifier,
         rgiAssessmentSrvc,
         rgiUserListSrvc,
-        rgiIdentitySrvc,
-        rgiAssessmentMethodSrvc
+        rgiIdentitySrvc
     ) {
         // filtering options
         $scope.current_user = rgiIdentitySrvc.currentUser;
@@ -22,86 +18,47 @@ angular
             {value: 'status', text: 'Sort by Status'}];
         $scope.sort_order = $scope.sort_options[0].value;
 
-        switch ($scope.current_user.role) {
-            case 'supervisor':
-                $scope.sort_options.push({value: 'year', text: 'Sort by Year of assessment'});
-                $scope.sort_options.push({value: 'version', text: 'Sort by Version'});
+        var getAssessments = function(criteria) {
+            rgiAssessmentSrvc.query(criteria, function (assessments) {
+                $scope.assessments = [];
 
-                $scope.newAssessmentDialog = function () {
-                    rgiDialogFactory.assessmentNew($scope);
-                };
-                if (!$routeParams.version) {
-                    rgiAssessmentSrvc.query({}, function (assessments) {
-                        // pull assessment list from collection and adds user name to match reviewer id and researcher id
-                        var assessment;
-                        $scope.assessments = [];
-
-                        assessments.forEach(function (el) {
-                            assessment = el;
-                            if(el.last_modified) {
-                                assessment.edited_by = rgiUserListSrvc.get({_id: el.last_modified.modified_by});
-                            }
-                            if(assessment.researcher_ID) {
-                                assessment.researcher = rgiUserListSrvc.get({_id: assessment.researcher_ID});
-                            }
-                            if(assessment.reviewer_ID) {
-                                assessment.reviewer = rgiUserListSrvc.get({_id: assessment.reviewer_ID});
-                            }
-                            $scope.assessments.push(assessment);
-                        });
-                    });
-                } else {
-                    rgiAssessmentSrvc.query({
-                        year: $routeParams.version.substr(0, 4),
-                        version: $routeParams.version.substr(5)
-                    }, function (assessments) {
-                        // pull assessment list from collection and adds user name to match reviewer id and researcher id
-                        var assessment;
-                        $scope.assessments = [];
-
-                        assessments.forEach(function (el) {
-                            assessment = el;
-                            if (el.last_modified) {
-                                assessment.edited_by = rgiUserListSrvc.get({_id: el.last_modified.modified_by});
-                            }
-                            if(assessment.researcher_ID) {
-                                assessment.researcher = rgiUserListSrvc.get({_id: assessment.researcher_ID});
-                            }
-                            if(assessment.reviewer_ID) {
-                                assessment.reviewer = rgiUserListSrvc.get({_id: assessment.reviewer_ID});
-                            }
-                            $scope.assessments.push(assessment);
-                        });
-                    });
-                }
-                break;
-            case 'researcher':
-                rgiAssessmentSrvc.query({researcher_ID: $scope.current_user._id}, function (assessments) {
-                    // pull assessment list from collection and adds user name to match reviewer id and researcher id
-                    var assessment;
-                    $scope.assessments = [];
-                    assessments.forEach(function (assessment) {
+                assessments.forEach(function (assessment) {
+                    if (assessment.last_modified) {
                         assessment.edited_by = rgiUserListSrvc.get({_id: assessment.last_modified.modified_by});
+                    }
+
+                    if(assessment.researcher_ID) {
                         assessment.researcher = rgiUserListSrvc.get({_id: assessment.researcher_ID});
-                        if (assessment.reviewer_ID) {
-                            assessment.reviewer = rgiUserListSrvc.get({_id: assessment.reviewer_ID});
-                        }
-                        $scope.assessments.push(assessment);
-                    });
-                });
-                break;
-            case 'reviewer':
-                rgiAssessmentSrvc.query({reviewer_ID: $scope.current_user._id}, function (assessments) {
-                    // pull assessment list from collection and adds user name to match reviewer id and researcher id
-                    var assessment;
-                    $scope.assessments = [];
-                    assessments.forEach(function (assessment) {
-                        assessment.edited_by = rgiUserListSrvc.get({_id: assessment.last_modified.modified_by});
-                        assessment.researcher = rgiUserListSrvc.get({_id: assessment.researcher_ID});
+                    }
+
+                    if(assessment.reviewer_ID) {
                         assessment.reviewer = rgiUserListSrvc.get({_id: assessment.reviewer_ID});
-                        $scope.assessments.push(assessment);
-                    });
+                    }
+
+                    $scope.assessments.push(assessment);
                 });
-                break;
+            });
+        };
+
+        var criteria = {};
+
+        if ($scope.current_user.role === 'supervisor') {
+            $scope.sort_options.push({value: 'year', text: 'Sort by Year of assessment'});
+            $scope.sort_options.push({value: 'version', text: 'Sort by Version'});
+
+            if ($routeParams.version) {
+                criteria = {
+                    year: $routeParams.version.substr(0, 4),
+                    version: $routeParams.version.substr(5)
+                };
+            }
+        } else {
+            criteria[$scope.current_user.role + '_ID'] = $scope.current_user._id;
         }
+
+        getAssessments(criteria);
+
+        $scope.newAssessmentDialog = function () {
+            rgiDialogFactory.assessmentNew($scope);
+        };
     });
