d28d7cba7ef1435882461632cefb50d09333a026
Alex Elgin <sascha.elgin@gmail.com>
2016-02-17 15:59:34 +0600

    remove reference

--- public/app/directives/controllers/rgiBiblioFormCtrl.js
+++ public/app/directives/controllers/rgiBiblioFormCtrl.js
@@ -4,7 +4,9 @@ angular
     .module('app')
     .controller('rgiBiblioFormCtrl', function (
         $scope,
-        rgiDialogFactory
+        rgiAnswerMethodSrvc,
+        rgiDialogFactory,
+        rgiNotifier
     ) {
         $scope.ref_type = [
             {text: 'Add Document', value: 'document'},
@@ -18,6 +20,28 @@ angular
 
         resetSelection();
         $scope.$on('RESET_SELECTED_REFERENCE_ACTION', resetSelection);
+
+        $scope.deleteReference = function(referenceId) {
+            var removeReferenceIndex = -1;
+
+            for(var i in $scope.answer.references) {
+                if($scope.answer.references.hasOwnProperty(i) && $scope.answer.references[i]._id === referenceId) {
+                    removeReferenceIndex = i;
+                }
+            }
+
+            if(removeReferenceIndex > -1) {
+                var referencesBackup = $scope.answer.references.slice(0);
+                $scope.answer.references.splice(removeReferenceIndex, 1);
+
+                rgiAnswerMethodSrvc.updateAnswer($scope.answer).then(function () {
+                    rgiNotifier.notify('The reference has been deleted');
+                }, function (reason) {
+                    rgiNotifier.error(reason);
+                    $scope.answer.references = referencesBackup;
+                });
+            }
+        };
 
         //TODO Generate Dialog based on change and handle upload process via dialogs
         $scope.selectRefDialog = function(value) {
