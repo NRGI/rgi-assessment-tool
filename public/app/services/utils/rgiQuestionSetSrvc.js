1b759441f01c207383b4f51afc8aed634126fdf7
Alex Elgin <sascha.elgin@gmail.com>
2016-03-01 10:28:07 +0600

    fix navigation to the next question on answer submission

--- public/app/services/utils/rgiQuestionSetSrvc.js
+++ public/app/services/utils/rgiQuestionSetSrvc.js
@@ -1,14 +1,75 @@
 'use strict';
 
-angular.module('app').factory('rgiQuestionSetSrvc', function (rgiUtilsSrvc) {
-    var answers = [];
+angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, rgiUtilsSrvc) {
+    var answers = [], questions;
+
+    rgiQuestionSrvc.query({assessment_ID: 'base'}, function (questionList) {
+        questions = questionList;
+    });
+
+    var getNextQuestion = function(answer) {
+        var linkedQuestion = getLinkedQuestion(getQuestionOption(answer));
+        return linkedQuestion === undefined ? getRootQuestions()[0] : linkedQuestion;
+    };
+
+    var getLinkedQuestion = function(option) {
+        var foundQuestion;
+
+        questions.forEach(function(question) {
+            if(question.linkedOption === option._id) {
+                foundQuestion = question;
+            }
+        });
+
+        return foundQuestion;
+    };
+
+    var getQuestionOption = function(answer) {
+        var foundOption;
+
+        questions.forEach(function(question) {
+            if(answer.root_question_ID === question._id) {
+                question.question_criteria.forEach(function(option) {
+                    if(answer.researcher_score.text === option.text) {
+                        foundOption = option;
+                    }
+                });
+            }
+        });
+
+        return foundOption;
+    };
+
+    var getRootQuestions = function() {
+        var rootQuestions = [];
+
+        questions.forEach(function(question) {
+            if((question.linkedOption === undefined) && !isQuestionAnswered(question)) {
+                rootQuestions.push(question);
+            }
+        });
+
+        return rootQuestions;
+    };
+
+    var isQuestionAnswered = function(question) {
+        var answered = false;
+
+        answers.forEach(function(answer) {
+            if((answer.root_question_ID === question._id) && answer.researcher_score) {
+                answered = true;
+            }
+        });
+
+        return answered;
+    };
 
     return {
         isAnyQuestionRemaining: function(answer) {
-            return answer.question_order !== answers.length;
+            return getNextQuestion(answer) !== undefined;
         },
         getNextQuestionId: function(answer) {
-            return String(rgiUtilsSrvc.zeroFill((answer.question_order + 1), 3));
+            return String(rgiUtilsSrvc.zeroFill((getNextQuestion(answer).question_order), 3));
         },
         setAnswers: function(answersData) {
             answers = answersData;
