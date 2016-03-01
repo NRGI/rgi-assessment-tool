19a3db6e448c6d1e6c26b3e6aa1062e558184043
Alex Elgin <sascha.elgin@gmail.com>
2016-03-01 10:28:07 +0600

    fix navigation to the next question on answer submission

--- public/app/services/utils/rgiQuestionSetSrvc.js
+++ public/app/services/utils/rgiQuestionSetSrvc.js
@@ -8,8 +8,13 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
     });
 
     var getNextQuestion = function(answer) {
+//<<<<<<< 60e02369f0ae31e5de028b81b974875ae9ac9a8d
         var linkedQuestion = getLinkedQuestion(getQuestionSelectedOption(answer));
         return linkedQuestion === undefined ? getRootQuestions(false)[0] : linkedQuestion;
+//=======
+//        var linkedQuestion = getLinkedQuestion(getQuestionOption(answer));
+//        return linkedQuestion === undefined ? getRootQuestions()[0] : linkedQuestion;
+//>>>>>>> fix navigation to the next question on answer submission
     };
 
     var getLinkedQuestion = function(option) {
@@ -24,6 +29,7 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
         return foundQuestion;
     };
 
+//<<<<<<< 60e02369f0ae31e5de028b81b974875ae9ac9a8d
     var getSelectedOption = function(answer) {
         return answer.researcher_score;
     };
@@ -35,6 +41,15 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
             if(isAssociatedQuestion(answer, question)) {
                 question.question_criteria.forEach(function(option) {
                     if(getSelectedOption(answer).text === option.text) {
+//=======
+//    var getQuestionOption = function(answer) {
+//        var foundOption;
+//
+//        questions.forEach(function(question) {
+//            if(answer.root_question_ID === question._id) {
+//                question.question_criteria.forEach(function(option) {
+//                    if(answer.researcher_score.text === option.text) {
+//>>>>>>> fix navigation to the next question on answer submission
                         foundOption = option;
                     }
                 });
@@ -44,11 +59,19 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
         return foundOption;
     };
 
+//<<<<<<< 60e02369f0ae31e5de028b81b974875ae9ac9a8d
     var getRootQuestions = function(showAnsweredQuestions) {
         var rootQuestions = [];
 
         questions.forEach(function(question) {
             if((question.linkedOption === undefined) && (!isQuestionAnswered(question) || showAnsweredQuestions)) {
+//=======
+//    var getRootQuestions = function() {
+//        var rootQuestions = [];
+//
+//        questions.forEach(function(question) {
+//            if((question.linkedOption === undefined) && !isQuestionAnswered(question)) {
+//>>>>>>> fix navigation to the next question on answer submission
                 rootQuestions.push(question);
             }
         });
@@ -76,7 +99,11 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
         var answered = false;
 
         answers.forEach(function(answer) {
+//<<<<<<< 60e02369f0ae31e5de028b81b974875ae9ac9a8d
             if(isAssociatedQuestion(answer, question) && getSelectedOption(answer)) {
+//=======
+//            if((answer.root_question_ID === question._id) && answer.researcher_score) {
+//>>>>>>> fix navigation to the next question on answer submission
                 answered = true;
             }
         });
@@ -118,9 +145,14 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
         isAnyQuestionRemaining: function(answer) {
             return getNextQuestion(answer) !== undefined;
         },
+//<<<<<<< 60e02369f0ae31e5de028b81b974875ae9ac9a8d
         isAvailable: function(answer) {
             var question = getAssociatedQuestion(answer);
             return isQuestionFound(question, getRootQuestions(true)) || isQuestionFound(question, getAvailableLinkedQuestions());
+//=======
+//        getNextQuestionId: function(answer) {
+//            return String(rgiUtilsSrvc.zeroFill((getNextQuestion(answer).question_order), 3));
+//>>>>>>> fix navigation to the next question on answer submission
         },
         setAnswers: function(answersData) {
             answers = answersData;
