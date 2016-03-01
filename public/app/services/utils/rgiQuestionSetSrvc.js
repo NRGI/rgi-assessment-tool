c993a635166218fcb8f1215c56af6bda6c3d5071
Alex Elgin <sascha.elgin@gmail.com>
2016-03-01 12:33:58 +0600

    fix the service 'Question Set'

--- public/app/services/utils/rgiQuestionSetSrvc.js
+++ public/app/services/utils/rgiQuestionSetSrvc.js
@@ -28,7 +28,7 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
         var foundOption;
 
         questions.forEach(function(question) {
-            if(answer.root_question_ID === question._id) {
+            if(isQuestionAssociated(answer, question)) {
                 question.question_criteria.forEach(function(option) {
                     if(answer.researcher_score.text === option.text) {
                         foundOption = option;
@@ -52,11 +52,15 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
         return rootQuestions;
     };
 
+    var isQuestionAssociated = function(answer, question) {
+        return answer.question_ID._id === question._id;
+    };
+
     var isQuestionAnswered = function(question) {
         var answered = false;
 
         answers.forEach(function(answer) {
-            if((answer.root_question_ID === question._id) && answer.researcher_score) {
+            if(isQuestionAssociated(answer, question) && answer.researcher_score) {
                 answered = true;
             }
         });
@@ -65,11 +69,11 @@ angular.module('app').factory('rgiQuestionSetSrvc', function (rgiQuestionSrvc, r
     };
 
     return {
-        isAnyQuestionRemaining: function(answer) {
-            return getNextQuestion(answer) !== undefined;
-        },
         getNextQuestionId: function(answer) {
             return String(rgiUtilsSrvc.zeroFill((getNextQuestion(answer).question_order), 3));
+        },
+        isAnyQuestionRemaining: function(answer) {
+            return getNextQuestion(answer) !== undefined;
         },
         setAnswers: function(answersData) {
             answers = answersData;
