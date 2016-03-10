ec6f41638c4b3c7a0f6fecb8c57681189ecaee6b
Alex Elgin <sascha.elgin@gmail.com>
2016-03-10 15:08:26 +0600

    remove useless filter

--- public/app/common/rgiFilters.js
+++ public/app/common/rgiFilters.js
@@ -8,24 +8,6 @@ angular.module('app')
             }
         };
     })
-    .filter('orderObjectBy', function() {
-        return function(items, field, reverse) {
-            var filtered = [];
-            angular.forEach(items, function(item) {
-                filtered.push(item);
-
-            });
-            filtered.sort(function (a, b) {
-                return (a[field] > b[field] ? 1 : -1);
-            });
-
-            if(reverse) {
-                filtered.reverse();
-            }
-
-            return filtered;
-        };
-    })
     .filter('role', function () {
         return function (role) {
             return role === undefined ? undefined : role.replace('ext_', 'external ');
