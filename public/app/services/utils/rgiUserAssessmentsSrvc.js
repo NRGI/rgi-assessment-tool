212f079fc7aa22d57b200379fe8f16018b45f44f
Alex Elgin <sascha.elgin@gmail.com>
2016-03-14 13:49:42 +0600

    add a service to manage user assessment links

---
+++ public/app/services/utils/rgiUserAssessmentsSrvc.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('app').factory('rgiUserAssessmentsSrvc', function (rgiUserMethodSrvc) {
+    var updateUser = function(user) {
+        return rgiUserMethodSrvc.updateUser(user).$promise;
+    };
+
+    return {
+        add: function(user, assessment) {
+            user.assessments.push({
+                assessment_ID: assessment.assessment_ID,
+                country_name: assessment.country
+            });
+            return updateUser(user);
+        },
+        remove: function(user, assessment) {
+            var assessmentIndex = -1;
+
+            user.assessments.forEach(function(userAssessment, i) {
+                if (assessment.assessment_ID === userAssessment.assessment_ID) {
+                    if (i > -1) {
+                        assessmentIndex = i;
+                    }
+                }
+            });
+
+            user.assessments.splice(assessmentIndex, 1);
+            return updateUser(user);
+        }
+    };
+});
