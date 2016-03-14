2ce9023bda42d8fe86047fe4035b0ca35a500347
Chris Perry <byndcivilization@gmail.com>
2016-03-14 18:14:21 -0400

    finished doc part of ref update #286

--- public/app/dialogs/references/rgiEditRefDialogCtrl.js
+++ public/app/dialogs/references/rgiEditRefDialogCtrl.js
@@ -10,23 +10,18 @@ angular.module('app')
     ) {
         var new_answer_data = $scope.$parent.$parent.answer,
             ref_index = $scope.$parent.$parent.ref_index;
-        $scope.ref = new_answer_data.references[ref_index]
-        //console.log(new_answer_data);
-        //console.log(ref_index);
-        //console.log(new_answer_data.references[ref_index]);
+        $scope.ref = new_answer_data.references[ref_index];
 
         $scope.refEdit = function(ref_index) {
             console.log(ref_index)
-
-            //
-            //new_answer_data.references[ref_index].hidden = true;
-            //rgiAnswerMethodSrvc.updateAnswer(new_answer_data).then(function () {
-            //    $scope.closeDialog();
-            //    $rootScope.$broadcast('RESET_REFERENCE_ACTION');
-            //    rgiNotifier.notify('The reference has been deleted');
-            //}, function (reason) {
-            //    rgiNotifier.error(reason);
-            //});
+            new_answer_data.references[ref_index] = $scope.ref;
+            rgiAnswerMethodSrvc.updateAnswer(new_answer_data).then(function () {
+                $scope.closeDialog();
+                $rootScope.$broadcast('RESET_REFERENCE_ACTION');
+                rgiNotifier.notify('The reference has been edited');
+            }, function (reason) {
+                rgiNotifier.error(reason);
+            });
         };
         $scope.closeDialog = function () {
             ngDialog.close();
