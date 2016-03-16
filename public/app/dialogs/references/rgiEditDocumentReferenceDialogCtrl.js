956ce6ea57e1ff7c7e8f1b8b155e2cb491c55cdc
Alex Elgin <sascha.elgin@gmail.com>
2016-03-16 17:20:55 +0600

    show calendar in the dialog 'Edit Document Reference'

--- public/app/dialogs/references/rgiEditDocumentReferenceDialogCtrl.js
+++ public/app/dialogs/references/rgiEditDocumentReferenceDialogCtrl.js
@@ -14,6 +14,21 @@ angular.module('app')
 
         $scope.ref = new_answer_data.references[ref_index];
 
+        var today = new Date();
+        $scope.date_default = today;
+        $scope.date_max_limit = today;
+        $scope.date_options = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+        $scope.date_formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.date_format = $scope.date_formats[0];
+        $scope.status = {opened: false};
+
+        $scope.openCalendar = function () {
+            $scope.status.opened = true;
+        };
+
         $scope.editReference = function(referenceIndex) {
             new_answer_data.references[referenceIndex] = $scope.ref;
             rgiAnswerMethodSrvc.updateAnswer(new_answer_data).then(function () {
