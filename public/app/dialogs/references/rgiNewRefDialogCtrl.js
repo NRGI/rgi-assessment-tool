168812f233ca54e76f132a78ce6e8c83ca989ce9
Alex Elgin <sascha.elgin@gmail.com>
2016-03-21 17:45:11 +0600

    refactor the feature 'New Reference' using the calendar directive

--- public/app/dialogs/references/rgiNewRefDialogCtrl.js
+++ public/app/dialogs/references/rgiNewRefDialogCtrl.js
@@ -33,22 +33,6 @@ angular.module('app')
             {text: 'Expert', value: 'expert'},
             {text: 'Other', value: 'other'}];
         $scope.salutation_opts = ['mr.', 'mrs.', 'ms.'];
-        //Datepicker opts
-        var today = new Date();
-        $scope.date_default = today;
-        $scope.date_max_limit = today;
-        $scope.date_options = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-        $scope.date_formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.date_format = $scope.date_formats[0];
-        $scope.status = {
-            opened: false
-        };
-        $scope.calendarOpen = function () {
-            $scope.status.opened = true;
-        };
 
         $scope.answer_update = $scope.$parent.answer;
         $scope.interviewee_list = [];
