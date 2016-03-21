9ea600c93be28f38ad47d0ddc188342257c6130f
Alex Elgin <sascha.elgin@gmail.com>
2016-03-21 17:44:02 +0600

    refactor the feature 'Edit Reference' using the calendar directive

--- public/app/dialogs/references/rgiEditReferenceDialogCtrl.js
+++ public/app/dialogs/references/rgiEditReferenceDialogCtrl.js
@@ -20,21 +20,6 @@ angular.module('app')
             $scope.interviewee = rgiIntervieweeSrvc.get({_id: intervieweeId});
         }
 
-        var today = new Date();
-        $scope.date_default = today;
-        $scope.date_max_limit = today;
-        $scope.date_options = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-        $scope.date_formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.date_format = $scope.date_formats[0];
-        $scope.status = {opened: false};
-
-        $scope.openCalendar = function () {
-            $scope.status.opened = true;
-        };
-
         $scope.editReference = function(referenceIndex) {
             new_answer_data.references[referenceIndex] = $scope.ref;
             rgiAnswerMethodSrvc.updateAnswer(new_answer_data).then(function () {
