eadde221125e6671ea4f9fbba3ce6689f27138e8
Alex Elgin <sascha.elgin@gmail.com>
2016-03-22 18:52:13 +0600

    hide justification comment of an external reviewer if an alternative score is selected

--- public/app/directives/controllers/rgiExternalAnswerTabsCtrl.js
+++ public/app/directives/controllers/rgiExternalAnswerTabsCtrl.js
@@ -7,4 +7,9 @@ angular.module('app')
         rgiIdentitySrvc
     ) {
         $scope.current_user = rgiIdentitySrvc.currentUser;
+
+        $scope.isAlternativeScore = function(score) {
+            return ($scope.answer.researcher_score.value !== score.value) &&
+                ($scope.answer.reviewer_score.value !== score.value);
+        };
     });
