da9854be854954d6fb32100b2da412b005151889
Alex Elgin <sascha.elgin@gmail.com>
2016-03-24 12:37:07 +0600

    add resource processor

---
+++ public/app/services/utils/rgiResourceProcessorSrvc.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('app').factory('rgiResourceProcessorSrvc', function ($q) {
+    var resourceProcessor = {
+        getResponseHandler: function(dfd) {
+            return function (response) {
+                if(response.reason) {
+                    dfd.reject(response.reason);
+                } else {
+                    dfd.resolve(response);
+                }
+            };
+        },
+        getErrorHandler: function(dfd) {
+            return function () {
+                dfd.reject('Request Failed');
+            };
+        },
+        process: function(object, method) {
+            var dfd = $q.defer();
+            object[method]().then(resourceProcessor.getResponseHandler(dfd), resourceProcessor.getErrorHandler(dfd));
+            return dfd.promise;
+        },
+        delete: function(className, objectId) {
+            var object = new className();
+            object.id = objectId;
+            return resourceProcessor.process(object, '$delete');
+        }
+    };
+
+    return resourceProcessor;
+});
