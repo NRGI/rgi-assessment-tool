1d0a7dd9b9962aed6e4d2ef140b6016e79ee5edb
Alex Elgin <sascha.elgin@gmail.com>
2016-03-24 12:51:29 +0600

    cache always GET User request

--- public/app/services/rgiUserListSrvc.js
+++ public/app/services/rgiUserListSrvc.js
@@ -3,9 +3,6 @@
 angular.module('app')
     .factory('rgiUserListSrvc', function ($resource) {
         return $resource('/api/user-list/:_id', {_id: "@id"}, {
-            getCached: {
-                method : 'GET',
-                cache : true
-            }
+            get: {method : 'GET', cache: true}
         });
     });
