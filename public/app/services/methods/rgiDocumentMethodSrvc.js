26c628e6b3fa6f4dd24ccc349c22a6ae9024fa99
Alex Elgin <sascha.elgin@gmail.com>
2016-03-24 12:37:55 +0600

    fix back-end response processing for documents

--- public/app/services/methods/rgiDocumentMethodSrvc.js
+++ public/app/services/methods/rgiDocumentMethodSrvc.js
@@ -1,36 +1,16 @@
 'use strict';
 
 angular.module('app')
-    .factory('rgiDocumentMethodSrvc', function ($q, rgiDocumentSrvc) {
+    .factory('rgiDocumentMethodSrvc', function (
+        rgiDocumentSrvc,
+        rgiResourceProcessorSrvc
+    ) {
         return {
-            updateDocument: function (new_doc_data) {
-                var dfd = $q.defer();
-
-                new_doc_data.$update().then(function () {
-                    dfd.resolve();
-                }, function (response) {
-                    dfd.reject(response.data.reason);
-                });
-                return dfd.promise;
+            updateDocument: function (doc) {
+                return rgiResourceProcessorSrvc.process(doc, '$update');
             },
             deleteDocument: function (documentId) {
-                var
-                    dfd = $q.defer(),
-                    doc = new rgiDocumentSrvc();
-
-                doc.id = documentId;
-
-                doc.$delete().then(function (response) {
-                    if(response.reason) {
-                        dfd.reject(response.reason);
-                    } else {
-                        dfd.resolve(response);
-                    }
-                }, function () {
-                    dfd.reject('Request Failed');
-                });
-
-                return dfd.promise;
+                return rgiResourceProcessorSrvc.delete(rgiDocumentSrvc, documentId);
             }
         };
     });
