116640b1304ea02bdc67de272a7641cc638fb09b
Chris Perry <byndcivilization@gmail.com>
2016-03-30 14:53:01 -0400

    allowing rtf files

--- public/app/services/utils/rgiAllowedFileExtensionGuideSrvc.js
+++ public/app/services/utils/rgiAllowedFileExtensionGuideSrvc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app').factory('rgiAllowedFileExtensionGuideSrvc', function () {
-    var allowedExtensions = ['csv', 'doc', 'docx', 'jpeg', 'jpg', 'pdf', 'png', 'xlsx'];
+    var allowedExtensions = ['csv', 'doc', 'docx', 'jpeg', 'jpg', 'pdf', 'png', 'xlsx', 'rtf'];
 
     return {
         getList: function() {
