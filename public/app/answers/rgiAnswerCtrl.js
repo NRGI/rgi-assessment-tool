0380225d06b64296a55ba6df8f4707e0bffd961a
Alex Elgin <sascha.elgin@gmail.com>
2016-04-01 10:46:53 +0600

    check whether the answer data are loaded

--- public/app/answers/rgiAnswerCtrl.js
+++ public/app/answers/rgiAnswerCtrl.js
@@ -56,12 +56,12 @@ angular.module('app')
         };
 
         $scope.isChoiceChanged = function() {
-            return ($scope.answer.new_answer_selection !== undefined) && (originallySubmitted.score.letter !==
-                $scope.answer.question_ID.question_criteria[$scope.answer.new_answer_selection].letter);
+            return ($scope.answer !== undefined) && ($scope.answer.new_answer_selection !== undefined) &&
+                (originallySubmitted.score.letter !== $scope.answer.question_ID.question_criteria[$scope.answer.new_answer_selection].letter);
         };
 
         $scope.isJustificationChanged = function() {
-            var justification = $scope.answer[$scope.current_user.role + '_justification'];
+            var justification = $scope.answer ? $scope.answer[$scope.current_user.role + '_justification'] : undefined;
             return (justification !== undefined) && (justification !== originallySubmitted.justification);
         };
 
