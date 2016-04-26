4725ec4e40f418286d528b120cf00aa04acdba1f
Alex Elgin <sascha.elgin@gmail.com>
2016-04-26 18:56:58 +0600

    add authorization e2e test suite

--- tests/e2e/home.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Home Page', function() {
-  it('should contain a particular text', function() {
-    browser.driver.get('http://localhost:3030/');
-    var body = browser.driver.findElement(by.css('body'));
-    expect(body.getText()).toContain('NRGI Resource Governance Index Assessment Tool');
-  });
-});
