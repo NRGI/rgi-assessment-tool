c34360c088badf3d2685a5699e55f3e704e6466e
Alex Elgin <sascha.elgin@gmail.com>
2016-04-28 18:23:53 +0600

    add password pattern

--- public/app/services/utils/rgiPatternSet.js
+++ public/app/services/utils/rgiPatternSet.js
@@ -2,4 +2,5 @@
 
 angular.module('app')
     .constant('HUMAN_NAME_PATTERN', /^[a-zA-Z]+([\-\s]?[a-zA-Z]+)*$/)
+    .constant('PASSWORD_PATTERN', /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,8}$/)
 ;
