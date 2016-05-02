3f1df86ec1c6674ca806421d194b1f4d0c1d8c7b
Alex Elgin <sascha.elgin@gmail.com>
2016-05-02 18:19:52 +0600

    add version pattern

--- tests/unit/services/utils/rgiPatternSet.spec.js
+++ tests/unit/services/utils/rgiPatternSet.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('rgiPatternSet', function () {
-    var HUMAN_NAME_PATTERN, PASSWORD_PATTERN,
+    var HUMAN_NAME_PATTERN, PASSWORD_PATTERN, VERSION_PATTERN,
         currentPattern,
         testPattern = function(description, testedValue, expectedResult) {
             it(description, function () {
@@ -11,9 +11,10 @@ describe('rgiPatternSet', function () {
 
     beforeEach(module('app'));
 
-    beforeEach(inject(function(_HUMAN_NAME_PATTERN_, _PASSWORD_PATTERN_) {
+    beforeEach(inject(function(_HUMAN_NAME_PATTERN_, _PASSWORD_PATTERN_, _VERSION_PATTERN_) {
         HUMAN_NAME_PATTERN = _HUMAN_NAME_PATTERN_;
         PASSWORD_PATTERN = _PASSWORD_PATTERN_;
+        VERSION_PATTERN = _VERSION_PATTERN_;
     }));
 
     describe('HUMAN_NAME_PATTERN', function() {
@@ -41,5 +42,19 @@ describe('rgiPatternSet', function () {
         testPattern('declines a password without a digit', 'P@ssword', false);
         testPattern('declines a password without an upper-case character', 'p@ssw0rd', false);
         testPattern('declines a password without a lower-case character', 'P@SSW0RD', false);
+    });
+
+    describe('VERSION_PATTERN', function() {
+        beforeEach(function() {
+            currentPattern = VERSION_PATTERN;
+        });
+
+        testPattern('declines a string containing an alphabetical character', 'v1', false);
+        testPattern('declines a string containing a special character (not a dot)', '#1', false);
+        testPattern('declines a string without a leading version number', '.1', false);
+        testPattern('approves a string with a leading version number only', '1', true);
+        testPattern('approves a string with a leading version number and sub-version number', '1.1', true);
+        testPattern('approves a string with multiple subversion numbers', '1.1.2', true);
+        testPattern('declines a string with a alphabetical character in a sub-subversion number', '1.1.2b', false);
     });
 });
