b5b059750825fd29ced2d1ff91758ece3bc106a0
Alex Elgin <sascha.elgin@gmail.com>
2016-05-04 21:29:11 +0600

    prevent saving profile with mismatched passwords

--- public/app/account/rgiProfileCtrl.js
+++ public/app/account/rgiProfileCtrl.js
@@ -36,28 +36,33 @@ angular.module('app')
             }
         };
 
+        var updateUser = function(userData) {
+            rgiUserMethodSrvc.updateUser(userData).then(function () {
+                rgiNotifier.notify('Your user account has been updated');
+                $route.reload();
+            }, function (reason) {
+                rgiNotifier.error(reason);
+            });
+        };
+
         $scope.update = function () {
             var new_user_data = $scope.current_user;
+
             if (!new_user_data.firstName || !new_user_data.lastName) {
                 rgiNotifier.error('You must supply a first and last name!');
             } else if (!new_user_data.email) {
                 rgiNotifier.error('You must supply an email!');
             } else {
-                // check if password update exists and pass it in
                 if ($scope.password && $scope.password.length > 0) {
-                    if ($scope.password !== $scope.password_rep) {
-                        rgiNotifier.error('Passwords must match!');
-                    } else {
+                    if ($scope.password === $scope.password_rep) {
                         new_user_data.password = $scope.password;
+                        updateUser(new_user_data);
+                    } else {
+                        rgiNotifier.error('Passwords must match!');
                     }
+                } else {
+                    updateUser(new_user_data);
                 }
-                // use authorization service to update user data
-                rgiUserMethodSrvc.updateUser(new_user_data).then(function () {
-                    rgiNotifier.notify('Your user account has been updated');
-                    $route.reload();
-                }, function (reason) {
-                    rgiNotifier.error(reason);
-                });
             }
         };
     });
