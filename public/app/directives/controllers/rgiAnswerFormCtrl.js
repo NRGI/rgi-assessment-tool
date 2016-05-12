8cb9d41b015b90b8d7e7b3ac52bda8d5cb064d1a
Alex Elgin <sascha.elgin@gmail.com>
2016-05-12 16:46:08 +0600

    clean up the `prev-answer-form` directive

--- public/app/directives/controllers/rgiAnswerFormCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app')
-    .controller('rgiAnswerFormCtrl', function (
-        $scope,
-        rgiIdentitySrvc
-    ) {
-        $scope.current_user = rgiIdentitySrvc.currentUser;
-    });
--- public/app/directives/directives/rgi-answer-form.js
+++ public/app/directives/directives/rgi-answer-form.js
@@ -4,7 +4,6 @@ angular.module('app')
     .directive('rgiAnswerForm', function() {
         return {
             restrict: 'EA',
-            controller: 'rgiAnswerFormCtrl',
             scope: true,
             templateUrl: '/partials/directives/templates/rgi-answer-form'
         };
