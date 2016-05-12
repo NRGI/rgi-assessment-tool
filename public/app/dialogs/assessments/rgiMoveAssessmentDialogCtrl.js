4e00c70c02f562c59d9614e583bb09aeacfd4edd
Alex Elgin <sascha.elgin@gmail.com>
2016-05-12 11:29:55 +0600

    refactor the 'Move Assessment Dialog' controller

--- public/app/dialogs/assessments/rgiMoveAssessmentDialogCtrl.js
+++ public/app/dialogs/assessments/rgiMoveAssessmentDialogCtrl.js
@@ -10,69 +10,44 @@ angular.module('app')
             return user.firstName + " " + user.lastName + ' (' + user.role + ')';
         };
 
-        rgiUserSrvc.get({_id: $scope.$parent.assessment.edit_control}, function (control_profile) {
+        rgiUserSrvc.get({_id: $scope.$parent.assessment.edit_control}, function (profile) {
             var workflowOptions = [],
                 assessment = $scope.$parent.assessment,
-                stats = $scope.$parent.assessment_counters;
+                stats = $scope.$parent.assessment_counters,
+                addOption = function(text, value) {
+                    workflowOptions.push({text: text, value: value});
+                };
 
             if (assessment.status === 'trial_submitted') {
                 if (stats.flagged !== 0) {
-                    workflowOptions.push({
-                        text: 'Send back to ' + getUserInfo(control_profile) + ' for review.',
-                        value: control_profile.role + '_trial'
-                    });
+                    addOption('Send back to ' + getUserInfo(profile) + ' for review.', profile.role + '_trial');
                 } else if (stats.approved === stats.length) {
-                    workflowOptions.push({
-                        text: 'Allow ' + getUserInfo(control_profile) + ' to continue assessment.',
-                        value: 'assigned_' + control_profile.role
-                    });
+                    addOption('Allow ' + getUserInfo(profile) + ' to continue assessment.', 'assigned_' + profile.role);
                 }
             } else {
-                workflowOptions.push({
-                    text: 'Send back to ' + getUserInfo(control_profile) + ' for review.',
-                    value: 'review_' + control_profile.role
-                });
+                addOption('Send back to ' + getUserInfo(profile) + ' for review.', 'review_' + profile.role);
+                if ((profile.role === 'researcher') && (stats.flagged === 0) && assessment.reviewer_ID) {
+                    var reviewer = assessment.reviewer_ID;
 
-                if ((control_profile.role === 'researcher') && (stats.flagged === 0) && assessment.reviewer_ID) {
                     if (assessment.first_pass) {
-                        workflowOptions.push({
-                            text: 'Move to ' + getUserInfo(assessment.reviewer_ID) + '.',
-                            value: assessment.reviewer_ID.role + '_trial'
-                        });
+                        addOption('Move to ' + getUserInfo(reviewer) + '.', reviewer.role + '_trial');
                     } else {
-                        workflowOptions.push({
-                            text: 'Move to ' + getUserInfo(assessment.reviewer_ID) + '.',
-                            value: 'assigned_' + assessment.reviewer_ID.role
-                        });
+                        addOption('Move to ' + getUserInfo(reviewer) + '.', 'assigned_' + reviewer.role);
                     }
-                } else if ((control_profile.role === 'reviewer') && (stats.flagged === 0)) {
-                    workflowOptions.push({
-                        text: 'Move to ' + getUserInfo(assessment.researcher_ID) + '.',
-                        value: 'assigned_' + assessment.researcher_ID.role
-                    });
+                } else if ((profile.role === 'reviewer') && (stats.flagged === 0)) {
+                    var researcher = assessment.researcher_ID;
+                    addOption('Move to ' + getUserInfo(researcher) + '.', 'assigned_' + researcher.role);
                 }
             }
 
             if ((stats.flagged === 0) && (stats.finalized === stats.length) && (assessment.status !== 'approved')) {
-                workflowOptions.push({
-                    text: 'Approve assessment',
-                    value: 'approved'
-                });
+                addOption('Approve assessment', 'approved');
             }
 
             if (assessment.status === 'approved') {
-                workflowOptions.push({
-                    text: 'Move to internal review',
-                    value: 'internal_review'
-                });
-                workflowOptions.push({
-                    text: 'Move to external review',
-                    value: 'external_review'
-                });
-                workflowOptions.push({
-                    text: 'Final approval',
-                    value: 'final_approval'
-                });
+                addOption('Move to internal review', 'internal_review');
+                addOption('Move to external review', 'external_review');
+                addOption('Final approval', 'final_approval');
             }
 
             $scope.workflow_opts = workflowOptions;
