ba4fa51ec81fb1a8551108814e17f3463f3908df
Alex Elgin <sascha.elgin@gmail.com>
2016-05-12 11:30:45 +0600

    simplify the 'Move Assessment Dialog' controller

--- public/app/dialogs/assessments/rgiMoveAssessmentDialogCtrl.js
+++ public/app/dialogs/assessments/rgiMoveAssessmentDialogCtrl.js
@@ -26,28 +26,25 @@ angular.module('app')
                 }
             } else {
                 addOption('Send back to ' + getUserInfo(profile) + ' for review.', 'review_' + profile.role);
-                if ((profile.role === 'researcher') && (stats.flagged === 0) && assessment.reviewer_ID) {
-                    var reviewer = assessment.reviewer_ID;
 
-                    if (assessment.first_pass) {
-                        addOption('Move to ' + getUserInfo(reviewer) + '.', reviewer.role + '_trial');
-                    } else {
-                        addOption('Move to ' + getUserInfo(reviewer) + '.', 'assigned_' + reviewer.role);
+                if(stats.flagged === 0) {
+                    if (profile.role === 'reviewer') {
+                        var researcher = assessment.researcher_ID;
+                        addOption('Move to ' + getUserInfo(researcher) + '.', 'assigned_' + researcher.role);
+                    } else if ((profile.role === 'researcher') && assessment.reviewer_ID) {
+                        var reviewer = assessment.reviewer_ID;
+                        var status = assessment.first_pass ? reviewer.role + '_trial' : 'assigned_' + reviewer.role;
+                        addOption('Move to ' + getUserInfo(reviewer) + '.', status);
                     }
-                } else if ((profile.role === 'reviewer') && (stats.flagged === 0)) {
-                    var researcher = assessment.researcher_ID;
-                    addOption('Move to ' + getUserInfo(researcher) + '.', 'assigned_' + researcher.role);
                 }
-            }
-
-            if ((stats.flagged === 0) && (stats.finalized === stats.length) && (assessment.status !== 'approved')) {
-                addOption('Approve assessment', 'approved');
             }
 
             if (assessment.status === 'approved') {
                 addOption('Move to internal review', 'internal_review');
                 addOption('Move to external review', 'external_review');
                 addOption('Final approval', 'final_approval');
+            } else if ((stats.flagged === 0) && (stats.finalized === stats.length)) {
+                addOption('Approve assessment', 'approved');
             }
 
             $scope.workflow_opts = workflowOptions;
