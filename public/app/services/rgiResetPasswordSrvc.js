2bd9aced2b300841cbe38f096f89d0752508398e
Alex Elgin <sascha.elgin@gmail.com>
2016-05-19 17:43:39 +0600

    replace the custom HTTP request sender with $http in the 'Reset Password' service

--- public/app/services/rgiResetPasswordSrvc.js
+++ public/app/services/rgiResetPasswordSrvc.js
@@ -1,12 +1,13 @@
 'use strict';
+
 angular.module('app')
-    .factory('rgiResetPasswordSrvc', function (rgiRequestSubmitterSrvc) {
+    .factory('rgiResetPasswordSrvc', function ($http) {
         return {
             recover: function (email) {
-                return rgiRequestSubmitterSrvc.submit('/api/reset-password-token/add', {email: email});
+                return $http.post('/api/reset-password-token/add', {email: email});
             },
             reset: function (token, password) {
-                return rgiRequestSubmitterSrvc.submit('/api/reset-password-token/reset', {token: token, password: password});
+                return $http.post('/api/reset-password-token/reset', {token: token, password: password});
             }
         };
     });
