48a465030681cb9be0029ea136d628663b5339ff
Alex Elgin <sascha.elgin@gmail.com>
2016-05-20 18:48:11 +0600

    introduce a default HTTP failure handler

--- public/app/services/utils/rgiHttpResponseProcessorSrvc.js
+++ public/app/services/utils/rgiHttpResponseProcessorSrvc.js
@@ -2,30 +2,50 @@
 
 angular.module('app').factory('rgiHttpResponseProcessorSrvc', function (
     $location,
-    rgiIdentitySrvc
+    rgiIdentitySrvc,
+    rgiNotifier
 ) {
-    return {
-        getMessage: function(response, defaultMessage) {
-            switch (response.status) {
-                case 403:
-                    return 'Please, re-login';
-                default:
-                    if(defaultMessage !== undefined) {
-                        return defaultMessage;
-                    } else if(response.data.reason !== undefined) {
-                        return response.data.reason;
-                    } else if(response.data.length > 0) {
-                        return response.data;
-                    } else {
-                        return 'Unknown error occurred';
+    var handledFailuresNumber = 0,
+        httpResponseProcessor = {
+            getDefaultHandler: function(alternativeMessage) {
+                return function(response) {
+                    if(httpResponseProcessor.getHandledFailuresNumber() === 0) {
+                        rgiNotifier.error(httpResponseProcessor.getMessage(response, alternativeMessage));
+                        httpResponseProcessor.handle(response);
                     }
+                };
+            },
+            getHandledFailuresNumber: function() {
+                return handledFailuresNumber;
+            },
+            getMessage: function(response, defaultMessage) {
+                switch (response.status) {
+                    case 403:
+                        return 'Please, re-login';
+                    default:
+                        if(defaultMessage !== undefined) {
+                            return defaultMessage;
+                        } else if(response.data.reason !== undefined) {
+                            return response.data.reason;
+                        } else if(response.data.length > 0) {
+                            return response.data;
+                        } else {
+                            return 'Unknown error occurred';
+                        }
+                }
+            },
+            handle: function(response) {
+                handledFailuresNumber++;
+
+                if(response.status === 403) {
+                    rgiIdentitySrvc.currentUser = undefined;
+                    $location.path('/');
+                }
+            },
+            resetHandledFailuresNumber: function() {
+                handledFailuresNumber = 0;
             }
-        },
-        handle: function(response) {
-            if(response.status === 403) {
-                rgiIdentitySrvc.currentUser = undefined;
-                $location.path('/');
-            }
-        }
-    };
+        };
+
+    return httpResponseProcessor;
 });
