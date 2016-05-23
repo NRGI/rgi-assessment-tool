ed2cba120b4a707f656b138b347bda0c58a8ffe8
Alex Elgin <sascha.elgin@gmail.com>
2016-05-23 15:05:38 +0600

    remove unused services

--- public/app/services/rgiUploadSrvc.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('app')
-    .factory('rgiUploadSrvc', function ($resource) {
-        var UploadResource = $resource('/file-upload', {}, {});
-
-        return UploadResource;
-    });
