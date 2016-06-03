afd564af6589098565305c5c1ba03b16e374c9ce
Chris Perry <byndcivilization@gmail.com>
2016-06-03 10:31:19 -0400

    SAVEPOINT

--- server/config/passport.js
+++ server/config/passport.js
@@ -16,6 +16,19 @@ module.exports = function () {
             });
         }
     ));
+    // passport.use(new OAuth2Strategy({
+    //         authorizationURL: 'https://www.example.com/oauth2/authorize',
+    //         tokenURL: 'https://www.example.com/oauth2/token',
+    //         clientID: EXAMPLE_CLIENT_ID,
+    //         clientSecret: EXAMPLE_CLIENT_SECRET,
+    //         callbackURL: "http://localhost:3000/auth/example/callback"
+    //     },
+    //     function(accessToken, refreshToken, profile, cb) {
+    //         User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+    //             return cb(err, user);
+    //         });
+    //     }
+    // ));
 
     passport.serializeUser(function (user, done) {
         if (user) {
