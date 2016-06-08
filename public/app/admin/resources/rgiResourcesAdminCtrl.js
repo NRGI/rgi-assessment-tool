14306400f740a7e1bfc0aa48b462c6c0016177f0
Alex Elgin <sascha.elgin@gmail.com>
2016-06-08 16:18:37 +0600

    reorder the resources

--- public/app/admin/resources/rgiResourcesAdminCtrl.js
+++ public/app/admin/resources/rgiResourcesAdminCtrl.js
@@ -3,15 +3,20 @@
 angular.module('app')
     .controller('rgiResourcesAdminCtrl', function (
         $scope,
-        $route,
         $location,
+        $route,
+        $q,
+        _,
         rgiDialogFactory,
         rgiHttpResponseProcessorSrvc,
         rgiNotifier,
         rgiResourcesSrvc,
-        rgiResourcesMethodSrvc
+        rgiResourcesMethodSrvc,
+        rgiSortableGuideSrvc
     ) {
+        var originalResources = [];
         $scope.resource_type = $location.$$path.replace('/admin/', '').replace('-admin', '');
+        $scope.resources = [];
 
         if ($scope.resource_type === 'resource') {
             $scope.title = 'Other Resources';
@@ -21,8 +26,56 @@ angular.module('app')
 
         rgiResourcesSrvc.query({type: $scope.resource_type}, function(resources) {
             $scope.resources = resources;
+            originalResources = _.map(resources, _.clone);
+
+            $scope.resources.forEach(function(resource) {
+                resource.newOrder = resource.order;
+            });
         }, rgiHttpResponseProcessorSrvc.getDefaultHandler('Load ' +
             ($scope.resource_type === 'faq' ? 'FAQ' : $scope.resource_type) + ' data failure'));
+
+        $scope.sortableOptions = rgiSortableGuideSrvc.getOptions(function() {
+            $scope.resources.forEach(function(resource, index) {
+                resource.newOrder = index + 1;
+            });
+        });
+
+        $scope.reorder = function() {
+            var promises = [];
+
+            originalResources.forEach(function(originalResource) {
+                $scope.resources.forEach(function(resource) {
+                    if((resource._id === originalResource._id) && (resource.order !== resource.newOrder)) {
+                        originalResource.order = resource.newOrder;
+                        promises.push(rgiResourcesMethodSrvc.updateResource(originalResource).$promise);
+                    }
+                });
+            });
+
+            $q.all(promises).then(function() {
+                rgiNotifier.notify('Reorder completed');
+
+                originalResources.forEach(function(originalResource) {
+                    $scope.resources.forEach(function(resource) {
+                        if(resource._id === originalResource._id) {
+                            resource.order = originalResource.order;
+                        }
+                    });
+                });
+            }, rgiHttpResponseProcessorSrvc.getDefaultHandler('Reorder failed'));
+        };
+
+        $scope.isOrderChanged = function() {
+            var orderChanged = false;
+
+            $scope.resources.forEach(function(resource) {
+                if(resource.order !== resource.newOrder) {
+                    orderChanged = true;
+                }
+            });
+
+            return orderChanged;
+        };
 
         $scope.updateResource = function (resource) {
             if (!resource.head) {
