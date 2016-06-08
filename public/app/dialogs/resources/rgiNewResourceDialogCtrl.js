a8a5b5271e2da9dcf9704b0bb39dd3f7d22c8013
Alex Elgin <sascha.elgin@gmail.com>
2016-06-08 18:01:25 +0600

    fix the 'New Resource' dialog

--- public/app/dialogs/resources/rgiNewResourceDialogCtrl.js
+++ public/app/dialogs/resources/rgiNewResourceDialogCtrl.js
@@ -4,7 +4,6 @@ angular.module('app')
     .controller('rgiNewResourceDialogCtrl', function (
         $scope,
         $route,
-        ngDialog,
         rgiNotifier,
         rgiResourcesMethodSrvc,
         rgiIdentitySrvc
@@ -22,20 +21,16 @@ angular.module('app')
             } else if (!$scope.new_resource.head) {
                 rgiNotifier.error('You must supply a title!');
             } else if (!$scope.new_resource.body) {
-                rgiNotifier.error('You must supply content!')
+                rgiNotifier.error('You must supply content!');
             } else {
                 rgiResourcesMethodSrvc.createResource($scope.new_resource)
                     .then(function () {
                         rgiNotifier.notify('New resource created!');
-                        ngDialog.close();
+                        $scope.closeThisDialog();
                         $route.reload();
                     }, function (reason) {
                         rgiNotifier.error(reason);
                     });
             }
-        };
-
-        $scope.closeDialog = function () {
-            ngDialog.close();
         };
     });
