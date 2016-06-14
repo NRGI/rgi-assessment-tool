887ed8bfb7b43c6b895071a9e2927e9bf74f31b7
Alex Elgin <sascha.elgin@gmail.com>
2016-06-14 16:15:09 +0600

    add a test suite for the 'Edit Document' dialog controller

---
+++ tests/unit/controllers/dialogs/references/rgiEditDocumentDialogCtrl.spec.js
@@ -0,0 +1,277 @@
+'use strict';
+
+describe('rgiEditDocumentDialogCtrl', function () {
+    beforeEach(module('app'));
+
+    var $scope, $route, ngDialog, rgiIdentitySrvc, rgiNotifier, rgiDocumentMethodSrvc,
+        identityCurrentUserBackup, CURRENT_USER = 'CURRENT USER', DOCUMENT = 'ORIGINAL DOCUMENT';
+
+    beforeEach(inject(
+        function ($rootScope, $controller, _$route_, _ngDialog_, _rgiIdentitySrvc_, _rgiNotifier_, _rgiDocumentMethodSrvc_) {
+            $route = _$route_;
+            ngDialog = _ngDialog_;
+            rgiIdentitySrvc = _rgiIdentitySrvc_;
+            rgiNotifier = _rgiNotifier_;
+            rgiDocumentMethodSrvc = _rgiDocumentMethodSrvc_;
+
+            identityCurrentUserBackup = _.clone(rgiIdentitySrvc.currentUser);
+            rgiIdentitySrvc.currentUser = CURRENT_USER;
+
+            $scope = $rootScope.$new();
+            $scope.$parent = {document: DOCUMENT};
+
+            $controller('rgiEditDocumentDialogCtrl', {$scope: $scope});
+        }
+    ));
+
+    it('sets the current user data', function() {
+        $scope.current_user.should.be.equal(CURRENT_USER);
+    });
+
+    it('sets the document data', function() {
+        $scope.new_doc_data.should.deep.equal(DOCUMENT);
+    });
+
+    it('initializes the document type list', function() {
+        $scope.doc_type.should.deep.equal([
+            {value: 'journal', text: 'Journal'},
+            {value: 'data_file', text: 'Data File'},
+            {value: 'book', text: 'Book'},
+            {value: 'generic', text: 'Generic'},
+            {value: 'book_section', text: 'Book Section'},
+            {value: 'conference_proceedings', text: 'Conference Proceedings'},
+            {value: 'working_paper', text: 'Working Paper'},
+            {value: 'report', text: 'Report'},
+            {value: 'web_page', text: 'Web Page'},
+            {value: 'thesis', text: 'Thesis'},
+            {value: 'magazine_article', text: 'Magazine Article'},
+            {value: 'statute', text: 'Statute'},
+            {value: 'patent', text: 'Patent'},
+            {value: 'parliamentary_meeting_note', text: 'Parliamentary meeting notes'},
+            {value: 'newspaper_article', text: 'Newspaper Article'},
+            {value: 'policy_document', text: 'Policy document'},
+            {value: 'computer_program', text: 'Computer Program'},
+            {value: 'hearing', text: 'Hearing'},
+            {value: 'television_broadcast', text: 'Television Broadcast'},
+            {value: 'encyclopedia_article', text: 'Encyclopedia Article'},
+            {value: 'case', text: 'Case'},
+            {value: 'film', text: 'Film'},
+            {value: 'statute', text: 'Statute'},
+            {value: 'bill', text: 'Bill'}
+        ]);
+    });
+
+    describe('#authorPush', function() {
+        it('adds author full name to the authors list', function() {
+            $scope.new_doc_data = {authors: []};
+            $scope.authorPush();
+            $scope.new_doc_data.authors.should.deep.equal([{first_name: '', last_name: ''}]);
+        });
+    });
+
+    describe('#authorPop', function() {
+        it('removes author by the specified index', function() {
+            $scope.new_doc_data = {authors: [
+                {first_name: 'Julia', last_name: 'Roberts'},
+                {first_name: 'Joel', last_name: 'Cohen'},
+                {first_name: 'Eric', last_name: 'Roberts'}
+            ]};
+
+            $scope.authorPop(1);
+            $scope.new_doc_data.authors.should.deep.equal([
+                {first_name: 'Julia', last_name: 'Roberts'},
+                {first_name: 'Eric', last_name: 'Roberts'}
+            ]);
+        });
+    });
+
+    describe('#editorPush', function() {
+        it('adds editor full name to the editors list', function() {
+            $scope.new_doc_data = {editors: []};
+            $scope.editorPush();
+            $scope.new_doc_data.editors.should.deep.equal([{first_name: '', last_name: ''}]);
+        });
+    });
+
+    describe('#editorPop', function() {
+        it('removes editor by the specified index', function() {
+            $scope.new_doc_data = {editors: [
+                {first_name: 'Julia', last_name: 'Roberts'},
+                {first_name: 'Joel', last_name: 'Cohen'},
+                {first_name: 'Eric', last_name: 'Roberts'}
+            ]};
+
+            $scope.editorPop(1);
+            $scope.new_doc_data.editors.should.deep.equal([
+                {first_name: 'Julia', last_name: 'Roberts'},
+                {first_name: 'Eric', last_name: 'Roberts'}
+            ]);
+        });
+    });
+
+    describe('#closeDialog', function() {
+        it('closes the dialog', function() {
+            var dialogMock = sinon.mock(ngDialog);
+            dialogMock.expects('close');
+
+            $scope.closeDialog();
+
+            dialogMock.verify();
+            dialogMock.restore();
+        });
+    });
+
+    describe('#documentSave', function() {
+        var mocks = {};
+
+        beforeEach(function() {
+            mocks.notifier = sinon.mock(rgiNotifier);
+        });
+
+        describe('INCOMPLETE DATA CASE', function() {
+            it('shows an error message if the title is not set', function() {
+                $scope.new_doc_data = {title: undefined};
+                mocks.notifier.expects('error').withArgs('You must provide a title!');
+            });
+
+            it('shows an error message if the title is not set', function() {
+                $scope.new_doc_data = {title: 'TITLE', type: undefined};
+                mocks.notifier.expects('error').withArgs('You must provide a document type!');
+            });
+
+            it('shows an error message if no publisher either author first name is set', function() {
+                $scope.new_doc_data = {
+                    title: 'TITLE',
+                    type: 'TYPE',
+                    publisher: false,
+                    authors: [{first_name: undefined}]
+                };
+                mocks.notifier.expects('error').withArgs('You must provide either a publisher or an author!');
+            });
+
+            it('shows an error message if no publisher either author last name is set', function() {
+                $scope.new_doc_data = {
+                    title: 'TITLE',
+                    type: 'TYPE',
+                    publisher: false,
+                    authors: [{first_name: 'FIRST NAME', last_name: undefined}]
+                };
+                mocks.notifier.expects('error').withArgs('You must provide either a publisher or an author!');
+            });
+
+            it('shows an error message if the year of publication is not set', function() {
+                $scope.new_doc_data = {
+                    title: 'TITLE',
+                    type: 'TYPE',
+                    publisher: true,
+                    authors: [{first_name: 'FIRST NAME', last_name: 'LAST NAME'}],
+                    year: false
+                };
+                mocks.notifier.expects('error').withArgs('You must provide the year of publication!');
+            });
+
+            afterEach(function() {
+                $scope.documentSave($scope.new_doc_data);
+                mocks.notifier.verify();
+            });
+        });
+
+        describe('COMPLETE DATA CASE', function() {
+            var documentMethodUpdateDocumentSpy, documentMethodUpdateDocumentStub,
+                setStub = function(callback) {
+                    documentMethodUpdateDocumentSpy = sinon.spy(function() {
+                        return {then: callback};
+                    });
+
+                    documentMethodUpdateDocumentStub = sinon.stub(rgiDocumentMethodSrvc, 'updateDocument',
+                        documentMethodUpdateDocumentSpy);
+                };
+
+            beforeEach(function() {
+                $scope.new_doc_data = {
+                    title: 'TITLE',
+                    type: 'TYPE',
+                    publisher: true,
+                    authors: [{first_name: 'FIRST NAME', last_name: 'LAST NAME'}],
+                    year: true
+                };
+            });
+
+            describe('SUCCESS CASE', function() {
+                beforeEach(function() {
+                    setStub(function(callback) {
+                        callback();
+                    });
+
+                    mocks.dialog = sinon.mock(ngDialog);
+                    mocks.dialog.expects('close');
+
+                    mocks.notifier.expects('notify').withArgs('Document has been updated');
+                    $scope.documentSave($scope.new_doc_data);
+                });
+
+                it('closes the dialog', function() {
+                    mocks.dialog.verify();
+                });
+
+                it('shows a notification message', function() {
+                    mocks.notifier.verify();
+                });
+            });
+
+            describe('FAILURE CASE', function() {
+                it('shows the failure reason', function() {
+                    var FAILURE_REASON = 'FAILURE REASON';
+
+                    setStub(function(callbackSuccess, callbackFailure) {
+                        callbackFailure(FAILURE_REASON);
+                    });
+
+                    mocks.notifier.expects('error').withArgs(FAILURE_REASON);
+                    $scope.documentSave($scope.new_doc_data);
+                    mocks.notifier.verify();
+                });
+            });
+
+            describe('SOURCE CHANGE', function() {
+                beforeEach(function() {
+                    setStub(function(callback) {
+                        callback();
+                    });
+                });
+
+                it('adds protocol to the source if the protocol is not set', function() {
+                    $scope.new_doc_data.source = 'google.com';
+                    $scope.documentSave($scope.new_doc_data);
+                    $scope.new_doc_data.source.should.be.equal('http://google.com');
+                });
+
+                it('does not modify the source protocol if the protocol is set to `http`', function() {
+                    $scope.new_doc_data.source = 'http://google.com';
+                    $scope.documentSave($scope.new_doc_data);
+                    $scope.new_doc_data.source.should.be.equal('http://google.com');
+                });
+
+                it('does not modify the source protocol if the protocol is set to `https`', function() {
+                    $scope.new_doc_data.source = 'https://google.com';
+                    $scope.documentSave($scope.new_doc_data);
+                    $scope.new_doc_data.source.should.be.equal('https://google.com');
+                });
+            });
+
+            afterEach(function() {
+                documentMethodUpdateDocumentSpy.withArgs($scope.new_doc_data).called.should.be.equal(true);
+            });
+        });
+
+        afterEach(function() {
+            Object.keys(mocks).forEach(function(mockName) {
+                mocks[mockName].restore();
+            });
+        });
+    });
+
+    afterEach(function() {
+        rgiIdentitySrvc.currentUser = identityCurrentUserBackup;
+    });
+});
