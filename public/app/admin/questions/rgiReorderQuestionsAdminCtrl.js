b3a517be8ef8b11c4adc91f9d7f41e731bf2eb73
Alex Elgin <sascha.elgin@gmail.com>
2016-06-23 15:34:28 +0600

    add 'Reorder Questions' feature

---
+++ public/app/admin/questions/rgiReorderQuestionsAdminCtrl.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('app')
+    .controller('rgiReorderQuestionsAdminCtrl', function (
+        $scope,
+        rgiHttpResponseProcessorSrvc,
+        rgiPreceptGuideSrvc,
+        rgiQuestionSrvc,
+        rgiSortableGuideSrvc
+    ) {
+        $scope.precepts = rgiPreceptGuideSrvc.getQuestionTemplates();
+
+        rgiQuestionSrvc.query({assessment_ID: 'base'}, function (questions) {
+            questions.forEach(function (question) {
+                $scope.precepts[question.precept - 1].data.push(question);
+            });
+
+            $scope.precepts.forEach(function(precept) {
+                precept.data.sort(function(questionA, questionB) {
+                    return questionA.question_order - questionB.question_order;
+                });
+            });
+
+            $scope.questions = questions;
+        }, rgiHttpResponseProcessorSrvc.getDefaultHandler('Load question data failure'));
+
+        $scope.sortableOptions = rgiSortableGuideSrvc.getOptions(function() {
+            var questionOrder = 0;
+
+            $scope.precepts.forEach(function(precept) {
+                precept.data.forEach(function(question) {
+                    question.question_order = questionOrder++;
+                });
+            });
+        });
+    });
