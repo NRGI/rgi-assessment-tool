5eb2b53fc5c4d6e5508a91d5aa2309456ed05f68
Alex Elgin <sascha.elgin@gmail.com>
2016-06-23 17:20:28 +0600

    add a test suite for the 'Sortable Guide' service

---
+++ tests/unit/services/utils/rgiSortableGuideSrvc.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('rgiSortableGuideSrvc', function () {
+    var rgiSortableGuideSrvc;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function(_rgiSortableGuideSrvc_) {
+        rgiSortableGuideSrvc = _rgiSortableGuideSrvc_;
+    }));
+
+    describe('#getOptions', function() {
+        var sortableOptions, orderChangedCallback;
+
+        beforeEach(function() {
+            orderChangedCallback = sinon.spy();
+            sortableOptions = rgiSortableGuideSrvc.getOptions(orderChangedCallback);
+        });
+
+        it('does not allow duplicates', function() {
+            sortableOptions.allowDuplicates.should.be.equal(false);
+        });
+
+        it('does not allow clones', function() {
+            sortableOptions.clone.should.be.equal(false);
+        });
+
+        it('sets a `order changed` event listener', function() {
+            sortableOptions.orderChanged();
+            orderChangedCallback.called.should.be.equal(true);
+        });
+
+        it('accepts reordering', function() {
+            sortableOptions.accept({itemScope: {sortableScope: {$id: 'the same'}}}, {$id: 'the same'})
+                .should.be.equal(true);
+        });
+
+        it('declines regrouping', function() {
+            sortableOptions.accept({itemScope: {sortableScope: {$id: 'first one'}}}, {$id: 'another one'})
+                .should.be.equal(false);
+        });
+    });
+});
