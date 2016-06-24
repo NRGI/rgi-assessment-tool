76952aef3354d6853ef0325feffffaa5516f8fec
Alex Elgin <sascha.elgin@gmail.com>
2016-06-24 13:31:08 +0600

    save modified question order

--- public/app/admin/questions/rgiReorderQuestionsAdminCtrl.js
+++ public/app/admin/questions/rgiReorderQuestionsAdminCtrl.js
@@ -3,9 +3,12 @@
 angular.module('app')
     .controller('rgiReorderQuestionsAdminCtrl', function (
         $scope,
+        $q,
         rgiHttpResponseProcessorSrvc,
         rgiPreceptGuideSrvc,
+        rgiNotifier,
         rgiQuestionSrvc,
+        rgiQuestionMethodSrvc,
         rgiSortableGuideSrvc
     ) {
         $scope.precepts = rgiPreceptGuideSrvc.getQuestionTemplates();
@@ -48,6 +51,32 @@ angular.module('app')
         });
 
         $scope.reorder = function() {
+            var promises = [];
 
+            $scope.precepts.forEach(function(precept) {
+                precept.data.forEach(function(question) {
+                    var questionModified = false;
+
+                    if(isModified(question, 'question_order', 'newOrder')) {
+                        question.question_order = question.newOrder;
+                        questionModified = true;
+                    }
+
+                    if(isModified(question, 'precept', 'newPrecept')) {
+                        question.precept = question.newPrecept;
+                        questionModified = true;
+                    }
+
+                    if(questionModified) {
+                        promises.push(rgiQuestionMethodSrvc.updateQuestion(question).$promise);
+                    }
+                });
+            });
+
+            $q.all(promises).then(function() {
+                rgiNotifier.notify('The questions have been reordered');
+            }, function(reason) {
+                rgiNotifier.error(reason);
+            });
         };
     });
