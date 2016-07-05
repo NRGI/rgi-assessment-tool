707642917258e6f55a4b0ab0bde30b36beb05da3
Alex Elgin <sascha.elgin@gmail.com>
2016-07-05 13:07:07 +0600

    filter documents by assessment

--- public/app/services/rgiDocumentSrvc.js
+++ public/app/services/rgiDocumentSrvc.js
@@ -3,6 +3,7 @@
 angular.module('app').factory('rgiDocumentSrvc', function ($resource) {
     return $resource('/api/documents/:limit/:skip/:_id', {_id: '@id', limit: "@limit", skip: "@skip"}, {
         query: {method:'GET', isArray: false},
+        queryCached: {method:'GET', isArray: false, cache: true},
         get: {method: 'GET', cache: true},
         update: {method: 'PUT', isArray: false}
     });
