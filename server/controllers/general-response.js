af3b8a92295403dd59df4e8e7b4fa55f2dd28193
Alex Elgin <sascha.elgin@gmail.com>
2016-07-20 15:30:46 +0600

    prepare the `general-response` back-end module for extension

--- server/controllers/general-response.js
+++ server/controllers/general-response.js
@@ -1,18 +1,18 @@
 'use strict';
 
-exports.respondError = function(err, res) {
+exports.respondError = function(res, err) {
     res.status(400);
-    return res.send({ reason: err.toString() });
+    return res.send({reason: err.toString()});
 };
 
-exports.respondStatus = function(statusCode, res) {
+exports.respondStatus = function(res, statusCode) {
     res.sendStatus(statusCode);
     return res.end();
 };
 
 exports.submit = function(req, res) {
     if (req.status !== undefined) {
-        return exports.respondStatus(req.status, res);
+        return exports.respondStatus(res, req.status);
     } else if (req.error !== undefined) {
         res.send({reason: req.error.toString()});
         return res.end();
