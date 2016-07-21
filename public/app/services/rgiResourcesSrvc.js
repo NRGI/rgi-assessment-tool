a100f8dfa05615dbf160741d8a6c8f76d4dddbf3
Alex Elgin <sascha.elgin@gmail.com>
2016-07-21 09:55:33 +0600

    refactor the 'Resources' service

--- public/app/services/rgiResourcesSrvc.js
+++ public/app/services/rgiResourcesSrvc.js
@@ -1,10 +1,7 @@
 'use strict';
 
-angular.module('app')
-    .factory('rgiResourcesSrvc', function ($resource) {
-        var ResourcesResource = $resource('/api/resources/:_id', {_id: '@id'}, {
-            update: {method: 'PUT', isArray: false}
-        });
-
-        return ResourcesResource;
+angular.module('app').factory('rgiResourcesSrvc', function ($resource) {
+    return $resource('/api/resources/:_id', {_id: '@id'}, {
+        update: {method: 'PUT', isArray: false}
     });
+});
