5580c6dae58eac0457416dac73ab0b16f6d6d5d4
Alex Elgin <sascha.elgin@gmail.com>
2016-07-21 16:00:01 +0600

    redirect to the 'Interviewee List' page if redirect flag is set

--- public/app/dialogs/interviewees/rgiDeleteIntervieweeDialogCtrl.js
+++ public/app/dialogs/interviewees/rgiDeleteIntervieweeDialogCtrl.js
@@ -1,11 +1,7 @@
 'use strict';
 
 angular.module('app')
-    .controller('rgiDeleteIntervieweeDialogCtrl', function (
-        $scope,
-        rgiIntervieweeMethodSrvc,
-        rgiNotifier
-    ) {
+    .controller('rgiDeleteIntervieweeDialogCtrl', function ($scope, $location, rgiIntervieweeMethodSrvc, rgiNotifier) {
         var getIntervieweeIndex = function(currentInterviewee) {
             var intervieweeIndex = -1;
 
@@ -22,7 +18,12 @@ angular.module('app')
             rgiIntervieweeMethodSrvc.deleteInterviewee($scope.interviewee._id).then(function() {
                 $scope.closeThisDialog();
                 rgiNotifier.notify('The interviewee has been deleted');
-                $scope.interviewees.splice(getIntervieweeIndex($scope.interviewee), 1);
+
+                if($scope.redirectToIntervieweeList) {
+                    $location.path('/admin/interviewees-admin');
+                } else {
+                    $scope.interviewees.splice(getIntervieweeIndex($scope.interviewee), 1);
+                }
             }, function(reason) {
                 rgiNotifier.error(reason);
             });
