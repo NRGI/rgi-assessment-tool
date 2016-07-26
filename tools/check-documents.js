08ba89b50770a0852fd65e9eeb0e54b7833b5f79
Alex Elgin <sascha.elgin@gmail.com>
2016-07-26 17:43:36 +0600

    prepare a scratch of the document checker

--- tools/check-documents.js
+++ tools/check-documents.js
@@ -3,27 +3,50 @@
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
+const PDFParser = require("pdf2json/pdfparser");
 
-fs.readFile(__dirname + '/input/' + process.env.NODE_ENV + '.json', 'utf8', function (err, data) {
+var processPdfDocument = function(documentUrl, validHandler, invalidHandler) {
+    var pdfParser = new PDFParser();
+    pdfParser.on("pdfParser_dataError", validHandler);
+    pdfParser.on("pdfParser_dataReady", invalidHandler);
+    pdfParser.loadPDF(documentUrl);
+};
+
+var getInvalidS3PdfDocumentHandler = function(pdfDocument) {
+    return function() {
+        console.error(pdfDocument.s3_url + ' HAS BEEN SUCCESSFULLY OPEN.');
+    };
+};
+
+var getValidS3PdfDocumentHandler = function(pdfDocument) {
+    return function() {
+        console.error(pdfDocument.s3_url + ' cannot be open.');
+    };
+};
+
+var handleNotPdfDocument = function(notPdfDocument) {
+    console.error(notPdfDocument.s3_url + ' is not a PDF file.');
+};
+
+var getInputFilePath = function(env) {
+    return __dirname + '/input/' + env + '.json';
+};
+
+fs.readFile(getInputFilePath(process.env.NODE_ENV), 'utf8', function (err, data) {
     if(err) {
         console.log('read document list error');
     } else {
-        var documents = JSON.parse(data);
-        var pdfDocuments = [], removeDocumentIndices = [];
+        var inputDocuments = JSON.parse(data);
 
-        documents.forEach(function(document, documentIndex) {
-            const urlComponents = url.parse(document.s3_url.replace('\'', ''));
+        inputDocuments.forEach(function(inputDocument) {
+            const urlComponents = url.parse(inputDocument.s3_url);
 
-            if(path.extname(urlComponents.pathname) === '.pdf') {
-                pdfDocuments.push(document);
-                removeDocumentIndices.push(documentIndex);
+            if(path.extname(urlComponents.pathname) !== '.pdf') {
+                handleNotPdfDocument(inputDocument);
+            } else {
+                processPdfDocument(inputDocument.s3_url, getValidS3PdfDocumentHandler(inputDocument),
+                    getInvalidS3PdfDocumentHandler(inputDocument));
             }
-        });
-
-        removeDocumentIndices.reverse();
-
-        removeDocumentIndices.forEach(function(documentIndex) {
-            documents.splice(documentIndex, 1);
         });
     }
 });
