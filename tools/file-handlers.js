5893a5d1ed871e62580aea8dbfbfd6a859f65a4b
Alex Elgin <sascha.elgin@gmail.com>
2016-07-30 21:48:09 +0600

    log categorization of the checked documents

--- tools/file-handlers.js
+++ tools/file-handlers.js
@@ -6,21 +6,27 @@ const https = require('https');
 const PDFParser = require('pdf2json/pdfparser');
 
 const filePath = require('./file-path');
+const logger = require('./logger');
 
 exports.handlePdfDocument = function(pdfDocument, field, getValidHandler, getInvalidHandler, callback) {
     var fileAbsolutePath = filePath.getDownloadFilePath(pdfDocument._id);
     var file = fs.createWriteStream(fileAbsolutePath);
     var documentDownloader = pdfDocument[field].indexOf('https://') > -1 ? https : http;
 
+    logger.log((field === 'source' ? 'get the source ' : 'get the S3 ') + ' of ' + pdfDocument._id);
+
     documentDownloader.get(pdfDocument[field], function(response) {
+        logger.log('download the PDF document ' + pdfDocument._id);
         response.pipe(file);
 
         file.on('finish', function() {
             file.close(function() {
+                logger.log('parse the PDF document ' + pdfDocument._id);
                 var pdfParser = new PDFParser();
                 pdfParser.on('pdfParser_dataReady', getValidHandler(pdfDocument, callback));
 
                 pdfParser.on('pdfParser_dataError', function() {
+                    logger.log('parse failure of the document ' + pdfDocument._id);
                     fs.unlink(fileAbsolutePath, getInvalidHandler(pdfDocument, callback));
                 });
 
@@ -28,6 +34,7 @@ exports.handlePdfDocument = function(pdfDocument, field, getValidHandler, getInv
             });
         });
     }).on('error', function() {
+        logger.log('download failure of the document ' + pdfDocument._id);
         handleUnprocessedPdfDocument(pdfDocument, callback);
     });
 };
@@ -46,29 +53,34 @@ exports.getInvalidS3PdfDocumentHandler = function(pdfDocument, callback) {
 exports.getValidS3PdfDocumentHandler = function(pdfDocument, callback) {
     return function() {
         fs.unlink(filePath.getDownloadFilePath(pdfDocument._id), function() {
+            logger.log(pdfDocument._id + ' is marked as VALID!');
             callback('valid', pdfDocument);
         });
     };
 };
 
 var handleInvalidS3PdfDocumentWithoutSource = function(pdfDocument, callback) {
+    logger.log(pdfDocument._id + ' is marked as SOURCE REQUIRED!');
     callback('source-required', pdfDocument);
 };
 
 var handleUnprocessedPdfDocument = function(pdfDocument, callback) {
     fs.unlink(filePath.getDownloadFilePath(pdfDocument._id), function() {
+        logger.log(pdfDocument._id + ' is marked as UNPROCESSED!');
         callback('unprocessed', pdfDocument);
     });
 };
 
 var getValidSourcePdfDocumentHandler = function(pdfDocument, callback) {
     return function() {
+        logger.log(pdfDocument._id + ' is marked as FIX-AVAILABLE!');
         callback('fix-available', pdfDocument);
     };
 };
 
 var getInvalidSourcePdfDocumentHandler = function(pdfDocument, callback) {
     return function() {
+        logger.log(pdfDocument._id + ' is marked as BROKEN-SOURCE!');
         callback('broken-source', pdfDocument);
     };
 };
