5fb09c728add768533c198337f0a1e35f0366203
Alex Elgin <sascha.elgin@gmail.com>
2016-07-30 21:44:35 +0600

    perform a single iteration only

--- tools/runner.js
+++ tools/runner.js
@@ -23,23 +23,18 @@ fs.readFile(filePath.getInputFilePath(), 'utf8', function (dataError, serialized
                     logger.initialize();
                 }
 
-                var processPortion = function() {
-                    if(config.processed < data.length) {
-                        var portion = data.slice(config.processed, config.processed + config.portion);
-
-                        documentsChecker.check(portion, config, function() {
-                            config.processed += portion.length;
-                            fs.writeFile(configFilePath, JSON.stringify(config), processPortion);
-
-                            logger.log('Processed ' + config.processed + ' of ' + data.length + ' i.e. ' +
-                                (config.processed / data.length * 100).toFixed(1) + '%');
-                        });
-                    }
-                };
-
-                processPortion();
                 logger.log('Already processed ' + config.processed + ' of ' + data.length + ' i.e. ' +
                     (config.processed / data.length * 100).toFixed(1) + '%');
+
+                var portion = data.slice(config.processed, config.processed + config.portion);
+
+                documentsChecker.check(portion, config, function() {
+                    config.processed += portion.length;
+                    fs.writeFile(configFilePath, JSON.stringify(config));
+
+                    logger.log('Processed ' + config.processed + ' of ' + data.length + ' i.e. ' +
+                        (config.processed / data.length * 100).toFixed(1) + '%');
+                });
             }
         });
     }
