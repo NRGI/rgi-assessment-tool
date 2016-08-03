379beb861b4c4fe9ea81ebcfcdb742c0b20c42d5
Alex Elgin <sascha.elgin@gmail.com>
2016-08-03 10:14:10 +0600

    move the service `Answer Raw` to an own file

--- public/app/services/rgiAnswerSrvc.js
+++ public/app/services/rgiAnswerSrvc.js
@@ -2,16 +2,7 @@
 
 angular.module('app')
     .factory('rgiAnswerSrvc', function ($resource) {
-        var AnswerResource = $resource('/api/answers/:answer_ID', {answer_ID: '@answer_ID'}, {
+        return $resource('/api/answers/:answer_ID', {answer_ID: '@answer_ID'}, {
             update: {method: 'PUT', isArray: false}
         });
-
-        return AnswerResource;
-    })
-    .factory('rgiAnswerRawSrvc', function ($resource) {
-        var AnswerRawResource = $resource('/api/raw_answers/:limit/:skip/:answer_ID', {_id: '@answer_ID', limit: "@limit", skip: "@skip"}, {
-            query: {method: 'GET', isArray: false, cache: true}
-        });
-
-        return AnswerRawResource;
     });
