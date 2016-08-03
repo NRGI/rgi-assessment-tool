b62120545910f4d35d7e935679c5ed53d6dcd613
Alex Elgin <sascha.elgin@gmail.com>
2016-08-03 18:17:51 +0600

    refactor the 'Answer Raw' service

--- public/app/services/rgiAnswerRawSrvc.js
+++ public/app/services/rgiAnswerRawSrvc.js
@@ -2,7 +2,7 @@
 
 angular.module('app')
     .factory('rgiAnswerRawSrvc', function ($resource) {
-        return $resource('/api/raw_answers/:limit/:skip/:answer_ID', {_id: '@answer_ID', limit: "@limit", skip: "@skip"}, {
+        return $resource('/api/raw_answers/:limit/:skip', {limit: "@limit", skip: "@skip"}, {
             query: {method: 'GET', isArray: true, cache: true}
         });
     });
