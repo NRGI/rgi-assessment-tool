b9b598096606c2d9ea771a1886176f6301180f82
Alex Elgin <sascha.elgin@gmail.com>
2016-08-05 10:55:18 +0600

    optimize the reference list

--- public/app/answers/rgiAnswerCtrl.js
+++ public/app/answers/rgiAnswerCtrl.js
@@ -47,7 +47,6 @@ angular.module('app')
 
         var resetReference = function() {
             rgiAnswerSrvc.get({answer_ID: $routeParams.answer_ID, assessment_ID: $routeParams.answer_ID.substring(0, 2)}, function (answer) {
-                console.log('refresh reference list');
                 $scope.references = answer.references;
             }, processFailureResponse);
         };
