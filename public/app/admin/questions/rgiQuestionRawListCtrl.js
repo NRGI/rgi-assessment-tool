9f5e01160a1a0f0e9b97b9950f72ae510377e78d
Alex Elgin <sascha.elgin@gmail.com>
2016-08-11 19:31:19 +0600

    implement exporting answer data

--- public/app/admin/questions/rgiQuestionRawListCtrl.js
+++ public/app/admin/questions/rgiQuestionRawListCtrl.js
@@ -6,7 +6,7 @@ angular.module('app')
         $scope,
         rgiQuestionRawSrvc
     ) {
-        $scope.header = [
+        $scope.questionListHeader = [
             'question_order',
             'question_v',
             'question_text',
@@ -26,6 +26,15 @@ angular.module('app')
                     $scope.questions = $scope.questions.concat(questions);
                     currentPage++;
                 }
+            },
+            getCriteriaFieldSet = function(question, field) {
+                var values = [];
+
+                question.question_criteria.forEach(function(criterion) {
+                    values.push(criterion[field]);
+                });
+
+                return JSON.stringify(values).split('"').join('\'');
             };
 
         rgiQuestionRawSrvc.query({skip: currentPage, limit: $scope.itemsPerPage}, addQuestions);
@@ -34,7 +43,24 @@ angular.module('app')
             var questions = [];
 
             $scope.questions.forEach(function(questionData) {
-                var question = {};
+                var question = {}, lastModified = questionData.last_modified, lastModifiedBy = lastModified.modified_by;
+
+                ['question_order', 'question_v', 'question_text'].forEach(function(field) {
+                    question[field] = questionData[field];
+                });
+
+                ['letter', 'text'].forEach(function(field) {
+                    question['question_criteria_' + field] = getCriteriaFieldSet(questionData, field);
+                });
+
+                if(lastModifiedBy.firstName && lastModifiedBy.lastName) {
+                    question.last_modified_modified_by = lastModifiedBy.firstName + ' ' + lastModifiedBy.lastName +
+                        (lastModifiedBy.email ? ' (' + lastModifiedBy.email + ')' : '');
+                } else {
+                    question.last_modified_modified_by = lastModifiedBy.email;
+                }
+
+                question.last_modified_modified_date = lastModified.modified_date;
                 questions.push(question);
             });
 
