6a0b5975f4a65b66309bea30016bb82d2a4e9da2
Alex Elgin <sascha.elgin@gmail.com>
2016-08-12 19:58:27 +0600

    build the exported question data on the back end

--- public/app/admin/questions/rgiQuestionRawListCtrl.js
+++ public/app/admin/questions/rgiQuestionRawListCtrl.js
@@ -1,71 +1,21 @@
 'use strict';
 
 angular.module('app')
-    .controller('rgiQuestionRawListCtrl', function (
-        _,
-        $scope,
-        rgiQuestionRawSrvc
-    ) {
-        $scope.questionListHeader = [
-            'question_order',
-            'question_v',
-            'question_text',
-            'question_criteria_letter',
-            'question_criteria_text',
-            'last_modified_modified_by',
-            'last_modified_modified_date'
-        ];
-
+    .controller('rgiQuestionRawListCtrl', function (_, $scope, rgiQuestionRawSrvc) {
         $scope.busy = false;
         $scope.questions = [];
-        $scope.itemsPerPage = 50;
+        $scope.portionSize = 50;
 
         var currentPage = 0, allItemsLoaded = false,
             addQuestions = function(questions) {
                 if(!questions.reason) {
-                    $scope.questions = $scope.questions.concat(questions);
+                    $scope.questions = $scope.questions.concat(questions.data);
+                    $scope.questionListHeader = questions.header;
                     currentPage++;
                 }
-            },
-            getCriteriaFieldSet = function(question, field) {
-                var values = [];
-
-                question.question_criteria.forEach(function(criterion) {
-                    values.push(criterion[field]);
-                });
-
-                return JSON.stringify(values).split('"').join('\'');
             };
 
-        rgiQuestionRawSrvc.query({skip: currentPage, limit: $scope.itemsPerPage}, addQuestions);
-
-        $scope.getExportedData = function() {
-            var questions = [];
-
-            $scope.questions.forEach(function(questionData) {
-                var question = {}, lastModified = questionData.last_modified, lastModifiedBy = lastModified.modified_by;
-
-                ['question_order', 'question_v', 'question_text'].forEach(function(field) {
-                    question[field] = questionData[field];
-                });
-
-                ['letter', 'text'].forEach(function(field) {
-                    question['question_criteria_' + field] = getCriteriaFieldSet(questionData, field);
-                });
-
-                if(lastModifiedBy.firstName && lastModifiedBy.lastName) {
-                    question.last_modified_modified_by = lastModifiedBy.firstName + ' ' + lastModifiedBy.lastName +
-                        (lastModifiedBy.email ? ' (' + lastModifiedBy.email + ')' : '');
-                } else {
-                    question.last_modified_modified_by = lastModifiedBy.email;
-                }
-
-                question.last_modified_modified_date = lastModified.modified_date;
-                questions.push(question);
-            });
-
-            return questions;
-        };
+        rgiQuestionRawSrvc.query({skip: currentPage, limit: $scope.portionSize}, addQuestions);
 
         $scope.loadMore = function () {
             if ($scope.busy) {
@@ -75,11 +25,11 @@ angular.module('app')
             $scope.busy = true;
 
             if(!allItemsLoaded) {
-                rgiQuestionRawSrvc.query({skip: currentPage, limit: $scope.itemsPerPage}).$promise
+                rgiQuestionRawSrvc.query({skip: currentPage, limit: $scope.portionSize}).$promise
                     .then(function (questions) {
                         addQuestions(questions);
 
-                        if (!questions.reason && (questions.length < $scope.itemsPerPage)) {
+                        if (!questions.reason && (questions.data.length < $scope.portionSize)) {
                             allItemsLoaded = true;
                         }
                     }).finally(function () {
--- public/app/services/rgiQuestionRawSrvc.js
+++ public/app/services/rgiQuestionRawSrvc.js
@@ -3,6 +3,6 @@
 angular.module('app')
     .factory('rgiQuestionRawSrvc', function ($resource) {
         return $resource('/api/raw-questions/:limit/:skip', {limit: "@limit", skip: "@skip"}, {
-            query: {method: 'GET', isArray: true, cache: true}
+            query: {method: 'GET', isArray: false, cache: true}
         });
     });
