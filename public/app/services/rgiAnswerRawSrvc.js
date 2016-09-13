5241c16bdc659c67002ee86250d68373bcafd679
Alex Elgin <sascha.elgin@gmail.com>
2016-09-13 23:23:10 +0600

    add country filter on the 'Raw Answer Data' page

--- public/app/services/rgiAnswerRawSrvc.js
+++ public/app/services/rgiAnswerRawSrvc.js
@@ -2,7 +2,7 @@
 
 angular.module('app')
     .factory('rgiAnswerRawSrvc', function ($resource) {
-        return $resource('/api/raw_answers/:limit/:skip', {limit: "@limit", skip: "@skip"}, {
+        return $resource('/api/raw_answers/:limit/:skip/:country', {country: '@country', limit: '@limit', skip: '@skip'}, {
             query: {method: 'GET', isArray: false, cache: true}
         });
     });
