bd547f6b99c2597e8c2b0554c143648147e0b133
Alex Elgin <sascha.elgin@gmail.com>
2016-10-18 12:34:36 +0600

    refactor the `Reference List` service

--- public/app/services/utils/rgiReferenceListSrvc.js
+++ public/app/services/utils/rgiReferenceListSrvc.js
@@ -1,37 +1,34 @@
 'use strict';
 
 angular.module('app').factory('rgiReferenceListSrvc', function () {
-    var
-        isAuthor = function(reference, user) {
-            if(user === undefined) {
-                return true;
-            } else {
-                return reference.author._id ? reference.author._id === user._id : reference.author === user._id;
-            }
-        },
-        isBelongingToUserType = function(reference, user) {
-            return (user !== undefined) && (reference.author.role !== undefined) && (reference.author.role === user.role);
-        },
-        getLength = function(references, user, checkReference) {
-            var length = 0;
+    var getLength = function(references, user, checkReference) {
+        var length = 0;
 
-            if(references !== undefined) {
-                references.forEach(function(reference) {
-                    if(!reference.hidden && checkReference(reference, user)) {
-                        length++;
-                    }
-                });
-            }
+        if(references !== undefined) {
+            references.forEach(function(reference) {
+                if(!reference.hidden && checkReference(reference, user)) {
+                    length++;
+                }
+            });
+        }
 
-            return length;
-        };
+        return length;
+    };
 
     var referenceList = {
         getLength: function(references, user) {
-            return getLength(references, user, isAuthor);
+            return getLength(references, user, function(reference, user) {
+                if(user === undefined) {
+                    return true;
+                } else {
+                    return reference.author._id ? reference.author._id === user._id : reference.author === user._id;
+                }
+            });
         },
         isAnyBelongingToTheUserType: function(references, user) {
-            return getLength(references, user, isBelongingToUserType);
+            return getLength(references, user, function(ref, user) {
+                return (user !== undefined) && (ref.author.role !== undefined) && (ref.author.role === user.role);
+            }) > 0;
         },
         isEmpty: function(references, user) {
             return referenceList.getLength(references, user) === 0;
