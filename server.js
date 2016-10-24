a3220c8d332b5e8f88b6b70811f87e9ca87b559a
Alex Elgin <sascha.elgin@gmail.com>
2016-10-24 15:14:06 +0600

    log the server bootstrapping logs using the logger wrapper

--- server.js
+++ server.js
@@ -3,23 +3,17 @@
 require('newrelic');
 
 var express         = require('express');
-
 var env             = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 var app             = express();
-
 var config          = require('./server/config/config')[env];
 
-
+var logger = require('./server/logger/logger');
 var user = process.env.DB_ID;
 var pass = process.env.DB_KEY;
 
 require('./server/config/express')(app, config, user, pass, env);
-
 require('./server/config/passport')();
-
 require('./server/config/routes')(app);
 
 app.listen(config.port);
-
-console.log('Listening on port ' + config.port + '...');
+logger.log('Listening on port ' + config.port + '...');
