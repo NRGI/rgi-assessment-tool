99ccac0edb2f6f7588697f0dc58c28778b299c23
Alex Elgin <sascha.elgin@gmail.com>
2016-10-24 15:09:52 +0600

    remove useless methods from the default logger

--- server/logger/logger.development.js
+++ server/logger/logger.development.js
@@ -1,13 +1,8 @@
 'use strict';
 
 module.exports = {
-    assert: console.assert,
-    dir: console.dir,
     error: console.error,
     info: console.info,
     log: console.log,
-    time: console.time,
-    timeEnd: console.timeEnd,
-    trace: console.trace,
     warn: console.warn
 };
