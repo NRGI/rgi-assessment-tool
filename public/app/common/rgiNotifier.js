76fc2d11c63bea22e217e6b0c7928692b6d75234
Alex Elgin <sascha.elgin@gmail.com>
2016-10-24 10:50:06 +0600

    integrate the logger service

--- public/app/common/rgiNotifier.js
+++ public/app/common/rgiNotifier.js
@@ -4,15 +4,15 @@ angular.module('app')
     .value('rgiToastr', toastr);
 
 angular.module('app')
-    .factory('rgiNotifier', function (rgiToastr) {
+    .factory('rgiNotifier', function (rgiLogger, rgiToastr) {
         return {
             notify: function (msg) {
                 rgiToastr.success(msg);
-                console.log(msg);
+                rgiLogger.log(msg);
             },
             error: function (msg) {
                 rgiToastr.error(msg);
-                console.log(msg);
+                rgiLogger.log(msg);
             }
         };
     });
