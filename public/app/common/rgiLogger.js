b4370b968ef3110c50dc56419c052ffe10590836
Alex Elgin <sascha.elgin@gmail.com>
2016-11-23 10:47:57 +0600

    Revert "submit client logs to the server"
    This reverts commit ded5f5b6c83e19de50b3156452e8b773a19f69d9.

--- public/app/common/rgiLogger.js
+++ public/app/common/rgiLogger.js
@@ -1,27 +1,3 @@
 'use strict';
 
-angular.module('app').factory('rgiLogger', ['$http', function ($http) {
-    var getMessage = function(messageList) {
-        var messages = [];
-
-        for(var messageIndex = 0; messageIndex < messageList.length; messageIndex++) {
-            messages.push(messageList[messageIndex]);
-        }
-
-        return messages.join(', ');
-    };
-
-    return {
-        assert: console.assert,
-        dir: console.dir,
-        error: console.error,
-        info: function() {
-            $http.post('/api/logs', {log: getMessage(arguments)});
-        },
-        log: console.log,
-        time: console.time,
-        timeEnd: console.timeEnd,
-        trace: console.trace,
-        warn: console.warn
-    };
-}]);
+angular.module('app').constant('rgiLogger', console);
--- server/config/routes.js
+++ server/config/routes.js
@@ -8,7 +8,6 @@ var auth                    = require('./auth'),
     countries               = require('../controllers/countries'),
     documents               = require('../controllers/documents'),
     interviewees            = require('../controllers/interviewees'),
-    logs                    = require('../controllers/logs'),
     precepts                = require('../controllers/precepts'),
     questions               = require('../controllers/questions'),
     resources               = require('../controllers/resources'),
@@ -105,8 +104,6 @@ module.exports = function (app) {
     app.get('/partials/*', function (req, res) {
         res.render('../../public/app/' + req.params[0]);
     });
-    //SUBMIT LOG
-    app.post('/api/logs', logs.post);
     // GET COUNTRY
     app.get('/api/countries', countries.getCountries);
     app.get('/api/countries/:country_ID', countries.getCountryByID);
