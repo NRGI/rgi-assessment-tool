0e21501c01c72fbffe9ab03e78f2a50c448857fb
Alex Elgin <sascha.elgin@gmail.com>
2016-12-16 22:15:52 +0600

    add `viewer` role

--- public/app/services/constants/rgiAvailableRolesSet.js
+++ public/app/services/constants/rgiAvailableRolesSet.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('app')
-    .constant('AVAILABLE_ROLES_SET', ['supervisor', 'researcher', 'reviewer', 'ext_reviewer'])
+    .constant('AVAILABLE_ROLES_SET', ['supervisor', 'viewer', 'researcher', 'reviewer', 'ext_reviewer'])
     .constant('ASSESSMENT_ROLES_SET', ['researcher', 'reviewer', 'ext_reviewer'])
 ;
